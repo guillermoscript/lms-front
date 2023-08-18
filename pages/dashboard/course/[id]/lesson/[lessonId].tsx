460a3b92023400a70fce5e8b8971a83934d4ef82
guillermoscript <arepayquezo@gmail.com>
2023-08-17 20:53:43 -0400

    simplify api call and now sidebar shows examn and homeworkd

--- pages/dashboard/course/[id]/lesson/[lessonId].tsx
+++ pages/dashboard/course/[id]/lesson/[lessonId].tsx
@@ -3,7 +3,7 @@ import { IndexPageRef, UserMeResponse } from '../../../../../utils/types/common'
 import PageTransition from '../../../../../components/PageTransition';
 import axios, { AxiosResponse } from 'axios';
 import { GetServerSidePropsContext } from 'next';
-import { Course, Lesson, Product, User } from '../../../../../payload-types';
+import { Course, Evaluation, Lesson, Product, User } from '../../../../../payload-types';
 import DashboardLayout from '../../../../../components/Dashboard/DashboardLayout';
 import { RichText } from '../../../../../components/RichText';
 import LessonCompleteForm from '../../../../../components/Lesson/LessonCompleteForm';
@@ -14,7 +14,7 @@ import { apiUrl } from '../../../../../utils/env';
 import { useCopyToClipboard } from 'usehooks-ts';
 import QAChatModal from '../../../../../components/AI/Modals/QAChatModal';
 import useBlockPrintScreen from '../../../../../utils/hooks/useBlockPrintScree';
-import ReactDOMServer from 'react-dom/server'
+import ReactDOMServer from 'react-dom/server';
 
 type LessonsProps = {
   data: Lesson;
@@ -22,38 +22,62 @@ type LessonsProps = {
   lessonId: string;
   courseId: string;
   lessons: Lesson[];
+  evaluations: Evaluation[];
 };
 
 function LessonsPage(props: LessonsProps, ref: IndexPageRef) {
-  
-  const { data, user, lessonId, courseId, lessons } = props;
-  const isCompletedByTheUser = data.completedBy?.some((otherUser) => typeof otherUser === 'object' && otherUser.id === user.id);
-  const [showModal , setShowModal] = useState<boolean>(false)
-  const [value, copy] = useCopyToClipboard()
-  useBlockPrintScreen();
+  const { data, user, lessonId, courseId, evaluations, lessons } = props;
+  const isCompletedByTheUser = data.completedBy?.some(
+    (otherUser) => typeof otherUser === 'object' && otherUser.id === user.id,
+  );
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [value, copy] = useCopyToClipboard();
   function removeTags(str: string) {
-    
     str = str.toString();
     // Regular expression to identify HTML tags in
     // the input string. Replacing the identified
     // HTML tag with a null string.
-    return str.replace( /(<([^>]+)>)/ig, '').replace(/\n/g, ' ')
-  }  
-
-  const plainText = removeTags(ReactDOMServer.renderToString(<RichText content={data.content} />))
+    return str.replace(/(<([^>]+)>)/gi, '').replace(/\n/g, ' ');
+  }
+  
+  const plainText = removeTags(ReactDOMServer.renderToString(<RichText content={data.content} />));
+  const exams = evaluations?.filter((value) => value.evaluationType === 'exam')
+  const homeworks = evaluations?.filter((value) => value.evaluationType === 'homework');
+  
+  useBlockPrintScreen();
 
   return (
     <>
       <PageTransition ref={ref}>
-        <DashboardLayout NavItems={
-          <DashboardNav 
-            iterable={lessons as Lesson[]}
-            link={`/dashboard/course/${data.id}/lesson`}
-            idOfParam="lessonId"
-            iterableName="Lecciones"
-          />
-        }>
-          
+        <DashboardLayout
+          NavItems={
+            <>
+              <DashboardNav
+                iterable={lessons as Lesson[]}
+                link={`/dashboard/course/${courseId}/lesson`}
+                idOfParam="lessonId"
+                iterableName="Lecciones"
+              />
+              {evaluations && (
+                <>
+                  <DashboardNav
+                    iterable={exams as Evaluation[]}
+                    link={`/dashboard/course/${courseId}/exam`}
+                    idOfParam="examnId"
+                    iterableName="Examenes"
+                  />
+                  <DashboardNav
+                    iterable={homeworks as Evaluation[]}
+                    link={`/dashboard/course/${courseId}/homework`}
+                    idOfParam="homeworkId"
+                    iterableName="Tareas"
+                  />
+                </>
+              )}
+            </>
+            
+          }
+        >
           {isCompletedByTheUser ? (
             <>
               <div className="indicator mb-4">
@@ -68,39 +92,45 @@ function LessonsPage(props: LessonsProps, ref: IndexPageRef) {
           <div className="flex flex-col">
             <RichText content={data.content} />
           </div>
-          
-          <button onClick={() => {
-            setShowModal(true)
-            copy(plainText)
-          }} className="btn btn-secondary">
+
+          <button
+            onClick={() => {
+              setShowModal(true);
+              copy(plainText);
+            }}
+            className="btn btn-secondary"
+          >
             Preguntar al Profe-bot 🤖
           </button>
 
           {!isCompletedByTheUser && (
-            <LessonCompleteForm lessonId={data.id} user={user} completedBy={typeof data.completedBy === 'object' ? data.completedBy : []} />
+            <LessonCompleteForm
+              lessonId={data.id}
+              user={user}
+              completedBy={typeof data.completedBy === 'object' ? data.completedBy : []}
+            />
           )}
-          {data.resources.length > 0 && (
+          {data?.resources?.length > 0 && (
+            <div className="flex flex-col">
+              <h2 className="text-3xl font-bold py-4">Recursos</h2>
               <div className="flex flex-col">
-                <h2 className="text-3xl font-bold py-4">Recursos</h2>
-                <div className="flex flex-col">
-                  {data.resources.map((resource) => {
-                    return <div className="flex flex-col" key={resource.id}>
+                {data.resources.map((resource) => {
+                  return (
+                    <div className="flex flex-col" key={resource.id}>
                       {/* <h3 className="text-2xl font-bold py-4">{resource.name}</h3> */}
                       <RichText content={resource.description} />
                     </div>
-                  })}
-                </div>
+                  );
+                })}
               </div>
-            )}
-          <Comments
-            commentableId={data.id}
-            relationTo='lessons'
-          />
+            </div>
+          )}
+          <Comments commentableId={data.id} relationTo="lessons" />
 
           {showModal && value && (
             <QAChatModal
               onClose={() => {
-                setShowModal(false)
+                setShowModal(false);
               }}
               value={value as string}
             />
@@ -113,68 +143,47 @@ function LessonsPage(props: LessonsProps, ref: IndexPageRef) {
 
 export async function getServerSideProps({ query, req }: GetServerSidePropsContext) {
   const { lessonId, id } = query;
-
-  try {
-    // const response = await payloadClient.get<Product>('/api/Lessons/' + lessonId,
-    const [response, lessonError] = await tryCatch<AxiosResponse<Lesson>>(
-      axios.get(apiUrl + '/api/lessons/' + lessonId, {
-        withCredentials: true,
-        headers: {
-          Authorization: `JWT ${req.cookies['payload-token']}`,
-        },
-      }),
-    );
-
-    const [UserMeResponse, userError] = await tryCatch<AxiosResponse<UserMeResponse>>(
-      axios.get(apiUrl + '/api/users/me', {
-        withCredentials: true,
-        headers: {
-          Authorization: `JWT ${req.cookies['payload-token']}`,
-        },
-      }),
-    );
-
-    const [courseEnrolled, courseEnrolledError] = await tryCatch<AxiosResponse<Course>>(axios.get(
-      apiUrl + '/api/enrollments/actives/' + id,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `JWT ${req.cookies['payload-token']}`,
-        },
+  const [course, courseError] = await tryCatch<AxiosResponse<Course>>(
+    axios.get<Course>(apiUrl + '/api/courses/' + id, {
+      withCredentials: true,
+      headers: {
+        Authorization: `JWT ${req.cookies['payload-token']}`,
       },
-    ));
+    }),
+  );
 
-    if (lessonError || userError || courseEnrolledError) {
-      console.log(lessonError);
-      console.log(userError);
-      console.log(courseEnrolledError);
-      return {
-        redirect: {
-              destination: '/auth/login',
-          permanent: false,
-        },
-      };
-    }
+  const [UserMeResponse, userError] = await tryCatch<AxiosResponse<UserMeResponse>>(
+    axios.get(apiUrl + '/api/users/me', {
+      withCredentials: true,
+      headers: {
+        Authorization: `JWT ${req.cookies['payload-token']}`,
+      },
+    }),
+  );
 
-    console.log();
-
+  if (courseError || userError) {
     return {
-      props: {
-        data: response?.data,
-        user: UserMeResponse?.data.user,
-        lessonId,
-        lessons: courseEnrolled?.data.lessons,
-        courseId: id,
-      }, // will be passed to the page component as props
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        data: null,
-      }, // will be passed to the page component as props
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      },
     };
   }
+
+  const lesson = course?.data?.lessons.find((lesson) => lesson.id === lessonId);
+  const lessons = course?.data?.lessons;
+  const evaluations = course?.data?.evaluations;
+
+  return {
+    props: {
+      data: lesson,
+      user: UserMeResponse?.data.user,
+      lessonId,
+      lessons,
+      evaluations,
+      courseId: id,
+    }, // will be passed to the page component as props
+  };
 }
 
 export default forwardRef(LessonsPage);
