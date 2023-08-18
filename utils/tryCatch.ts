827d4fbd108e1e0b2ae12e831b86fc27aad95c90
guillermoscript <arepayquezo@gmail.com>
2023-08-17 20:54:16 -0400

    honestly i dont know the change XD

--- utils/tryCatch.ts
+++ utils/tryCatch.ts
@@ -1,9 +1,9 @@
 export default async function tryCatch<T>(promise: Promise<T>): Promise<[T | null, Error | null]> {
-    try {  
-        const result = await promise
-        return [result, null]
+    try {
+        const result = await promise;
+        return [result, null];
     } catch (error) {
-        const er = error as Error
-        return [null, er]
+        const er = error as Error;
+        return [null, er];
     }
 }
