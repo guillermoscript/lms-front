645f1a7cde89cf582421fd3fec3b9792a881c9fb
guillermoscript <arepayquezo@gmail.com>
2023-11-11 17:50:25 -0400

    Update react-query imports and fix formatting

--- components/Examn/hooks/useMutationSubmitExamn.ts
+++ components/Examn/hooks/useMutationSubmitExamn.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "react-query";
+import { useMutation } from "@tanstack/react-query";
 import payloadClient from "../../../utils/axiosPayloadInstance"
 import { useRouter } from "next/router";
 
@@ -11,7 +11,7 @@ type useMutationSubmitExamn = {
     evaluation: string;
 }
 
-const postMutation = async ({formID, evaluation, submissionData}: useMutationSubmitExamn) => {
+const postMutation = async ({ formID, evaluation, submissionData }: useMutationSubmitExamn) => {
 
     console.log(formID, evaluation, submissionData)
     const res = await payloadClient.post(`/api/examns-submissions`, {
@@ -28,13 +28,14 @@ export default function useMutationSubmitExamn(handleSuccess: any) {
     const router = useRouter()
     const { examnId } = router.query
 
-    const mutation = useMutation((data: useMutationSubmitExamn) => {
-        return postMutation({
-            formID: data.formID,
-            submissionData: data.submissionData,
-            evaluation: examnId as string
-        })
-    }, {
+    const mutation = useMutation({
+        mutationFn: (data: useMutationSubmitExamn) => {
+            return postMutation({
+                formID: data.formID,
+                submissionData: data.submissionData,
+                evaluation: examnId as string
+            })
+        },
         onSuccess: (data) => {
             console.log(data)
             handleSuccess()
@@ -42,6 +43,7 @@ export default function useMutationSubmitExamn(handleSuccess: any) {
         onError: (error) => {
             console.log(error)
         }
+
     })
 
     return mutation
