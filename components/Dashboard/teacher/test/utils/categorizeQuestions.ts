c16bcac3692d1f18c21767f1172fdcbf160076e3
guillermoscript <arepayquezo@gmail.com>
2024-05-14 23:00:21 -0400

    refactor: Update categorizeQuestions function to handle question types and options
    The categorizeQuestions function in the utils/categorizeQuestions.ts file has been updated to handle different question types and options. This change improves the functionality of categorizing questions in the teacher dashboard.

--- components/Dashboard/teacher/test/utils/categorizeQuestions.ts
+++ components/Dashboard/teacher/test/utils/categorizeQuestions.ts
@@ -1,57 +1,48 @@
 function categorizeQuestions(questions: any[]) {
-    return questions.reduce(
-        (acc, question) => {
-            const localData = question.test_question_localizations[0]; // Using the first localization for simplicity
-            if (localData) {
-                const processedQuestion = {
-                    id: `q-${question.id}`,
-                    label: localData.question_text,
-                };
+    return questions.reduce((acc, question) => {
+        const processedQuestion = {
+            id: `q-${question.question_id}`,
+            label: question.question_text,
+        };
 
-                switch (question.question_type) {
-                    case "multiple_choices":
-                        const options = question.question_options.map((option: any) => {
-                            const optionLocal = option.question_option_localizations.find((loc: any) => loc.language_code === 'en');
-                            return {
-                                id: `opt-${option.id}`,
-                                text: optionLocal.option_text,
-                            };
-                        });
-                        acc.multipleChoiceQuestions.push({
-                            ...processedQuestion,
-                            options: options,
-                        });
-                        break;
-                    case "fill_in":
-                        acc.freeTextQuestions.push({
-                            ...processedQuestion,
-                            placeholder: "Enter your answer",
-                        });
-                        break;
-                    case "true_false":
-                        acc.singleSelectQuestions.push(
-                            {
-                                id: processedQuestion.id + "-true",
-                                text: `${processedQuestion.label} (True)`,
-                            },
-                            {
-                                id: processedQuestion.id + "-false",
-                                text: `${processedQuestion.label} (False)`,
-                            }
-                        );
-                        break;
-                    default:
-                        break;
-                }
-            }
-            return acc;
-        },
-        {
-            multipleChoiceQuestions: [],
-            freeTextQuestions: [],
-            singleSelectQuestions: [],
+        switch (question.question_type) {
+            case "multiple_choice":
+                const options = question.question_options.map((option: any) => ({
+                    id: `opt-${option.option_id}`,
+                    text: option.option_text,
+                }));
+                acc.multipleChoiceQuestions.push({
+                    ...processedQuestion,
+                    options: options,
+                });
+                break;
+            case "free_text":
+                acc.freeTextQuestions.push({
+                    ...processedQuestion,
+                    placeholder: "Enter your answer",
+                });
+                break;
+            case "true_false":
+                acc.singleSelectQuestions.push(
+                    {
+                        id: processedQuestion.id + "-true",
+                        text: `${processedQuestion.label} (True)`,
+                    },
+                    {
+                        id: processedQuestion.id + "-false",
+                        text: `${processedQuestion.label} (False)`,
+                    }
+                );
+                break;
+            default:
+                break;
         }
-    );
-};
+        return acc;
+    }, {
+        multipleChoiceQuestions: [],
+        freeTextQuestions: [],
+        singleSelectQuestions: [],
+    });
+}
 
 export default categorizeQuestions;
