13f4e7e2e31e7d97b9e1f2b5d175923d29bf6fa3
guillermoscript <arepayquezo@gmail.com>
2024-05-19 16:45:45 -0400

    refactor: Update selectClassNames in const.ts
    Update the selectClassNames object in the const.ts file to include a new 'label' property and modify the 'input' property. This change aligns the styling of the select input with the design requirements and improves the overall user experience.

--- utils/const.ts
+++ utils/const.ts
@@ -5,3 +5,10 @@ export const formClassNames = {
 	error: "pt-2 text-red-400",
 	container: "mb-4 flex flex-col gap-4",
 };
+
+export const selectClassNames = {
+	container: "mb-4 flex flex-col gap-4",
+	label: "text-xs my-2 font-medium text-neutral-600",
+	input: "flex my-2 h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+	error: "pt-2 text-red-400",
+}
