eb8fd3025804379d70d5be0d39cb8c41efc73c6f
guillermoscript <arepayquezo@gmail.com>
2024-05-19 16:47:22 -0400

    refactor: Update layout components for improved code organization and user experience
    Remove the Layout component from the checkout and store directories as they are no longer needed. This cleanup improves code organization and reduces unnecessary complexity. Additionally, add the Footer component to the Index page to improve the overall layout and provide consistent navigation across the application.

---
+++ app/(front)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<Header />
+			<>
+                {children}
+			</>
+            <Footer />
+		</>
+	);
+}
