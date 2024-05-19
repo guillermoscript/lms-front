7f9a0e4a82875b1512cebfafbc495d140e941616
guillermoscript <arepayquezo@gmail.com>
2024-05-19 16:45:21 -0400

    refactor: Add Footer component to Index page
    The Footer component has been added to the Index page to improve the overall layout and provide consistent navigation across the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,12 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { cookies } from "next/headers";
-import { DarkThemeToggle } from "@/components/DarkThemeToggle";
+
 
 export default async function Index() {
-  const cookieStore = cookies();
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
+  
 
   return (
+    <>
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Header>
       </Header>
@@ -193,5 +176,7 @@ export default async function Index() {
         </section>
       </div>
     </div>
+      <Footer />
+    </>
   );
 }
