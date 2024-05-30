7391a83d87a1cdac0cc5cb999b2d095432596f76
guillermoscript <arepayquezo@gmail.com>
2024-05-29 22:27:41 -0400

    feat: Update chat route to use Google's generative AI model
    This commit updates the chat route in the code to use Google's generative AI model from the @ai-sdk/google package. It replaces the previous model from the ollama-ai-provider package. The change improves the chat functionality by leveraging the capabilities of Google's generative AI model. The temperature for generating responses is set to 0 to ensure more deterministic and focused responses.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,13 +1,17 @@
-// import { openai } from '@ai-sdk/openai';
+import { openai } from '@ai-sdk/openai';
 import { StreamingTextResponse, streamText } from 'ai';
 import { ollama } from 'ollama-ai-provider';
+import { google } from '@ai-sdk/google';
 
 export async function POST(req: Request) {
     const { messages } = await req.json();
 
     const result = await streamText({
-        model: ollama('llama3'),
+        model: google('models/gemini-pro'),
+        // model: ollama('llama3'),
+        // model: openai('gpt-4o'),
         messages,
+        temperature: 0,
     });
 
     return new StreamingTextResponse(result.toAIStream());
