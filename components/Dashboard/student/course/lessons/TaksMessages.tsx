e28e3ec4a228e76666cf9a9fc87f756c409b280c
guillermoscript <arepayquezo@gmail.com>
2024-05-29 22:28:25 -0400

    feat: Update TaskMessages component to improve chat functionality
    This commit updates the TaskMessages component to improve the chat functionality. It adds the ability to display messages from the user and assistant, and includes features like displaying sender names, timestamps, and message content. Additionally, it adds icons for editing and copying messages for the user. This enhancement enhances the user experience and makes the chat interface more interactive and user-friendly.

--- components/Dashboard/student/course/lessons/TaksMessages.tsx
+++ components/Dashboard/student/course/lessons/TaksMessages.tsx
@@ -3,90 +3,139 @@
 import { Button } from "@/components/ui/button";
 import { ForwardRefEditor } from "@/components/ui/markdown/ForwardRefEditor";
 import ViewMarkdown from "@/components/ui/markdown/ViewMarkdown";
+import { cn } from "@/utils";
 import { useChat } from "ai/react";
+import { Copy, Pen } from "lucide-react";
 import { useEffect } from "react";
 
-export default function TaskMessages() {
-	const {
-		messages,
-		input,
-		handleInputChange,
-		setInput,
-		handleSubmit,
-		append,
-		setMessages,
-	} = useChat();
+const Message = ({
+	message,
+	sender,
+	time,
+	isUser,
+}: {
+	message: string;
+	sender: string;
+	time: string;
+	isUser: boolean;
+}) => {
+
+	if (sender !== 'user' && sender !== 'assistant') {
+		return null;
+	}
+
+	return (
+		<div
+			className={cn(
+				"flex w-full border-b p-1 mb-4",
+				isUser ? "justify-end " : "justify-start"
+			)}
+		>
+			<div className="flex items-end w-full">
+				{!isUser && (
+					<img
+						src={isUser ? "/asdasd/adad.png" : "/img/favicon.png"}
+						alt="profile"
+						className="max-w-[28px] object-cover rounded-full mr-4"
+					/>
+				)}
+
+				<div className="flex flex-col w-full px-4 py-2 rounded-lg relative">
+					<div className="font-bold mb-1 capitalize">
+						{sender}{" "}
+						<span className="text-xs text-gray-400 ml-2">
+							{time}
+						</span>
+					</div>
+
+					<ViewMarkdown markdown={message} />
+
+					{isUser && (
+						<div className="flex mt-2 space-x-2 ">
+							<Pen className="cursor-pointer" />
+
+							<Copy className="cursor-pointer" />
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const ChatWindow = ({ messages }: { messages: any[] }) => {
+	return (
+		<div className="flex-1 overflow-y-auto p-4 ">
+			{messages.map((msg, index) => (
+				<Message
+					key={index}
+					message={msg.content}
+					sender={msg.role}
+					time={msg.time}
+					isUser={msg.role === "user"}
+				/>
+			))}
+		</div>
+	);
+};
+
+const ChatInput = ({
+	handleSubmit,
+	handleInputChange,
+	input,
+}: {
+	handleSubmit: (e: any) => void;
+	input: string;
+	handleInputChange: (e: any) => void;
+}) => {
+	return (
+		<form onSubmit={handleSubmit} className="p-4 flex ">
+			{/* <ForwardRefEditor
+				className="flex-1 p-2 rounded-l-lg"
+				placeholder="Send a message"
+				markdown={message}
+				onChange={(value) => setMessage(value)}
+				onKeyDown={(e) => e.key === "Enter" && handleSend()}
+			/> */}
+			<input
+				type="text"
+				className="flex-1 p-2 border rounded-l-lg"
+				placeholder="Send a message"
+				onChange={handleInputChange}
+				value={input}
+			/>
+			<Button>Send</Button>
+		</form>
+	);
+};
+
+export default function TaskMessages({
+	systemPrompt,
+}: {
+	systemPrompt: string;
+}) {
+	const { messages, input, handleInputChange, handleSubmit, setMessages } =
+		useChat();
+	useChat();
 
 	useEffect(() => {
 		setMessages([
 			{
 				role: "system",
 				id: "1",
-				content: `Eres un profesore de castellano de universida muy importante y eres el mejor en castellano.
-                Le dejaste de tarea a tus estudiantes que escriban la siguiente frase de manera correcta: 
-                "yo nose manana si voy a komer o si no y tu si sabes yo kiero comer mucho comer comer comer".
-                Ellos te van a dar su propia respuesta a esto y si lo hcane mal le das la respuestas y explicas el como llegar a ella, si lo hacen bien los felicitas.
-                Si tus estudiantes responden con algo no relacionado, debes de afirmarle que tienen que responder la pregunta que se les hizo.
-            `,
+				content: systemPrompt,
 			},
 		]);
 	}, []);
 
 	return (
-		<div className="flex flex-col justify-center items-center h-screen border-t border-gray-700">
-			<div className="flex-1 w-full  overflow-auto p-6 space-y-4">
-				{messages.map((m, index) => (
-					<div
-						key={index}
-						className={`flex w-full ${
-							m.role === "user" ? "justify-end" : "justify-start"
-						}`}
-					>
-						<div
-							className={`rounded-lg p-4 text-sm max-w-[75%] ${
-								m.role === "user"
-									? "bg-gray-800 text-gray-200"
-									: m.role === "assistant"
-									? "bg-gray-700 text-gray-200"
-									: null
-							}`}
-						>
-							{
-                                m.role === 'user' || m.role === 'assistant' ? (
-                                    <ViewMarkdown markdown={m.content} />
-                                ) : null
-                            }
-						</div>
-					</div>
-				))}
-			</div>
-
-			<div className="w-full  p-4  rounded-lg">
-				<form
-					onSubmit={handleSubmit}
-					className="flex items-end gap-4 flex-col w-full"
-				>
-					<ForwardRefEditor
-						className="w-full rounded-lg p-4  border-none focus:ring-2 focus:ring-gray-500 focus:outline-none"
-						placeholder="Type your message..."
-						markdown={input}
-						onChange={(value) => setInput(value)}
-					/>
-
-					<Button
-						className="w-full"
-						variant="default"
-						type="submit"
-						onClick={() => {
-							append({ role: "user", content: input });
-
-							setInput("");
-						}}
-					>
-						Send
-					</Button>
-				</form>
-			</div>
+		<div className="min-h-screen flex flex-col">
+			<ChatWindow messages={messages} />
+			<ChatInput
+				handleSubmit={handleSubmit}
+				input={input}
+				handleInputChange={handleInputChange}
+			/>
 		</div>
 	);
 }
