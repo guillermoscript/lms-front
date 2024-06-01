f18dbee765524ce4ad54ffca71674e82763ab072
guillermoscript <arepayquezo@gmail.com>
2024-06-01 19:12:30 -0400

    refactor: Update sign-in and sign-up functions
    This refactor updates the sign-in and sign-up functions in the authActions.ts file. It improves the code readability by reorganizing the imports and removing unnecessary code comments. Additionally, it refactors the code to use consistent variable naming conventions and removes unused variables. These changes enhance the maintainability and clarity of the authentication functionality.

--- actions/auth/authActions.ts
+++ actions/auth/authActions.ts
@@ -1,66 +1,64 @@
-"use server";
+'use server'
 
-import { createResponse } from "@/utils/functions";
-import { createClient } from "@/utils/supabase/server";
-import { cookies, headers } from "next/headers";
-import { redirect } from "next/navigation";
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { createResponse } from '@/utils/functions'
+import { createClient } from '@/utils/supabase/server'
 
 export const signIn = async (prevData: any, formData: FormData) => {
-
-
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
     const supabase = createClient()
 
     const { data, error } = await supabase.auth.signInWithPassword({
         email,
-        password,
-    });
+        password
+    })
 
     if (error) {
-        console.log(error);
-        return createResponse('error', 'Invalid credentials', null, 'Invalid credentials');
+        console.log(error)
+        return createResponse('error', 'Invalid credentials', null, 'Invalid credentials')
     }
 
-    const userData = await supabase.from("user_roles").select("*").eq("user_id", data?.user?.id).single();
+    const userData = await supabase.from('user_roles').select('*').eq('user_id', data?.user?.id).single()
 
     if (userData?.error) {
         console
-        return createResponse('error', 'Error in sign in', null, userData.error.message);
+        return createResponse('error', 'Error in sign in', null, userData.error.message)
     }
 
-    const userRole = userData?.data.role_id;
+    const userRole = userData?.data.role_id
 
-    if (userRole === 1) {
-        return redirect("/dashboard");
-    } else if (userRole === 2) {
-        return redirect("/dashboard/teacher");
+    // forgive me for the following code
+    // TODO: refactor this
+    if (userRole === 37) {
+        return redirect('/dashboard')
+    } else if (userRole === 38) {
+        return redirect('/dashboard/teacher')
+    } else if (userRole === 39) {
+        return redirect('/dashboard/student')
     }
-
-    return redirect("/");
-
-};
-
+}
 
 export const signUp = async (prevData: any, formData: FormData) => {
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
+    const origin = headers().get('origin')
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
     const supabase = createClient()
 
     const { error } = await supabase.auth.signUp({
         email,
         password,
         options: {
-            emailRedirectTo: `${origin}/auth/callback`,
-        },
-    });
+            emailRedirectTo: `${origin}/auth/callback`
+        }
+    })
 
     if (error) {
-        console.log(error);
-        return createResponse('error', 'Error submitting comment', null, error);
+        console.log(error)
+        return createResponse('error', 'Error submitting comment', null, error)
     }
 
-    return redirect("/auth/login?message=Check email to continue sign in process");
-};
+    return redirect('/auth/login?message=Check email to continue sign in process')
+}
