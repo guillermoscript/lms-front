d1770926812181eab9e27b5e2eddfda1ec1064f4
guillermoscript <arepayquezo@gmail.com>
2024-06-04 18:19:39 -0400

    refactor: Update InitializedMDXEditor to include codeMirrorPlugin and sandpackPlugin
    This refactor updates the InitializedMDXEditor component in the InitializedMDXEditor.tsx file to include the codeMirrorPlugin and sandpackPlugin. These plugins enhance the functionality of the MDXEditor by adding support for code block editing with CodeMirror and integrating Sandpack for live coding experiences. This improvement provides users with a more robust and versatile markdown editing experience.

--- components/ui/markdown/InitializedMDXEditor.tsx
+++ components/ui/markdown/InitializedMDXEditor.tsx
@@ -3,9 +3,17 @@
 import '@mdxeditor/editor/style.css'
 
 import {
+    ChangeCodeMirrorLanguage,
     CodeBlockEditorDescriptor,
+    codeMirrorPlugin,
+    ConditionalContents,
+    InsertCodeBlock,
+    InsertSandpack,
     MDXEditorMethods,
-    MDXEditorProps
+    MDXEditorProps,
+    SandpackConfig,
+    sandpackPlugin,
+    ShowSandpackInfo
 } from '@mdxeditor/editor'
 import type { ForwardedRef } from 'react'
 
@@ -48,6 +56,33 @@ const PlainTextCodeEditorDescriptor: CodeBlockEditorDescriptor = {
     }
 }
 
+const defaultSnippetContent = `
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+`.trim()
+
+const simpleSandpackConfig: SandpackConfig = {
+    defaultPreset: 'react',
+    presets: [
+        {
+            label: 'React',
+            name: 'react',
+            meta: 'live react',
+            sandpackTemplate: 'react',
+            sandpackTheme: 'light',
+            snippetFileName: '/App.js',
+            snippetLanguage: 'jsx',
+            initialSnippetContent: defaultSnippetContent
+        }
+    ]
+}
+
 // Only import this to the next file
 export default function InitializedMDXEditor ({
     editorRef,
@@ -56,18 +91,24 @@ export default function InitializedMDXEditor ({
     return (
         <MDXEditor
             plugins={[
-			  codeBlockPlugin({
-			    codeBlockEditorDescriptors: [PlainTextCodeEditorDescriptor]
+                //   codeBlockPlugin({
+                //     codeBlockEditorDescriptors: [PlainTextCodeEditorDescriptor]
+                //     }),
+                // the default code block language to insert when the user clicks the "insert code block" button
+                codeBlockPlugin({ defaultCodeBlockLanguage: 'js' }),
+                sandpackPlugin({ sandpackConfig: simpleSandpackConfig }),
+                codeMirrorPlugin({
+                    codeBlockLanguages: { js: 'JavaScript', css: 'CSS', html: 'HTML', python: 'Python' }
                 }),
-			  headingsPlugin(),
-			  listsPlugin(),
-			  linkPlugin(),
-			  quotePlugin(),
-			  linkDialogPlugin(),
-			  tablePlugin(),
-			  markdownShortcutPlugin(),
-			  toolbarPlugin({
-			    toolbarContents: () => (
+                headingsPlugin(),
+                listsPlugin(),
+                linkPlugin(),
+                quotePlugin(),
+                linkDialogPlugin(),
+                tablePlugin(),
+                markdownShortcutPlugin(),
+                toolbarPlugin({
+                    toolbarContents: () => (
                         <>
                             <BlockTypeSelect />
                             <BoldItalicUnderlineToggles />
@@ -79,9 +120,29 @@ export default function InitializedMDXEditor ({
                             <UndoRedo />
                             <Separator />
                             <InsertTable />
+                            <ConditionalContents
+                                options={[
+                                    {
+                                        when: (editor) => editor?.editorType === 'codeblock',
+                                        contents: () => <ChangeCodeMirrorLanguage />
+                                    },
+                                    {
+                                        when: (editor) => editor?.editorType === 'sandpack',
+                                        contents: () => <ShowSandpackInfo />
+                                    },
+                                    {
+                                        fallback: () => (
+                                            <>
+                                                <InsertCodeBlock />
+                                                <InsertSandpack />
+                                            </>
+                                        )
+                                    }
+                                ]}
+                            />
                         </>
-			    )
-			  })
+                    )
+                })
             ]}
             {...props}
             ref={editorRef}
