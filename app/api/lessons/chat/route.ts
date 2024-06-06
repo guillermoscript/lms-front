87dac54ca6a2bf8ab254ff288ef1f871e2afe4c6
guillermoscript <arepayquezo@gmail.com>
2024-06-05 22:47:29 -0400

    feat: Add LessonComplete component for displaying lesson completion status

--- app/api/lessons/chat/route.ts
+++ app/api/lessons/chat/route.ts
@@ -2,100 +2,90 @@ import { google } from '@ai-sdk/google'
 import { convertToCoreMessages, streamText } from 'ai'
 import { z } from 'zod'
 
+import { createClient } from '@/utils/supabase/server'
+
 // Allow streaming responses up to 30 seconds
 export const dynamic = 'force-dynamic'
 export const maxDuration = 60
 
 export async function POST (req: Request) {
     // 'data' contains the additional data that you have sent:
-    const { messages, data } = await req.json()
 
-    const result = await streamText({
-        // model: openai('gpt-4-turbo'),
-        model: google('models/gemini-pro'),
-        onFinish: (event) => {
-            console.log('Finished', event)
-        },
-        // model: ollama('llama3'),
-        messages: convertToCoreMessages(messages),
-        system: `# You are an expert teacher in python.
+    try {
+        const {
+            messages, lessonId
+        } = await req.json()
+        const supabase = createClient()
+        console.log(lessonId)
 
-        As the expert teacher in python you have left an assignment for your students to complete. The assignment is to write a program that asks the user for their name, age, and favorite color, and then prints out this information. You have provided the students with the following instructions:
-        
-        **Assignment:** "Get to Know Me" Program
-        
-        **Objective:** Write a program that asks the user for their name, age, and favorite color, and then prints out this information.
-        
-        **Instructions:**
-        
-        1. Create a new Python file called "get_to_know_me.py".
-        2. Start by asking the user for their name. Use the "input()" function to get the user's response, and store it in a variable called "name". Make sure to use string data type (e.g., ""John"").
-        3. Next, ask the user for their age. Use the "int()" function to convert the user's response into an integer value. Store this value in a variable called "age".
-        4. Then, ask the user for their favorite color. Use the "input()" function again, and store the user's response in a variable called "favorite_color". Make sure to use string data type (e.g., ""blue"").
-        5. Finally, print out the user's name, age, and favorite color using the following format:
-        \`\`\`python
-        "My name is {name}, I am {age} years old, and my favorite color is {favorite_color}."
-        \`\`\`
-        Your Job is to review the code written by the students and provide feedback on their work.
-        
-        ### You must follow the following guidelines:
+        const result = await streamText({
+            // model: openai('gpt-4-turbo'),
+            model: google('models/gemini-pro'),
+            onFinish: async (event) => {
+                const userData = await supabase.auth.getUser()
+                if (userData.error) {
+                    console.log('Error getting user data', userData.error)
+                } else {
+                    const id = userData.data.user.id
+                    // add message to the database
+                    const message = await supabase.from('lessons_ai_task_messages').insert(
+                        {
+                            user_id: id,
+                            message: event.text,
+                            sender: 'assistant', // 'student' or 'assistant
+                            lesson_id: lessonId
+                        })
 
-        - YOU MUST NOT GIVE FULL ANSWERS TO THE STUDENTS. you must only provide feedback on their work.
-        - if the code is not working, provide feedback on the code bugs.
-        - Provide any other feedback that you think would be helpful for the students.
-        - If the given answer has nothing to do with the question, give appropriate feedback.
-        - If the given answer is correct, give appropriate feedback.
-        - If the given answer is partially correct, give appropriate feedback.
-        - If the given answer is correct but not well explained, give appropriate feedback.
-        - you can give hints to the students to help them understand the problem, but only with short and clear messages.
-        - you can ask the students to provide more information if needed.
-        
-        ## Example:
+                    if (message.error) {
+                        console.log(message.error)
+                    }
 
-        \`\`\`python
+                    console.log('Message added to the database', message)
+                }
+            },
+            // model: ollama('llama3'),
+            messages: convertToCoreMessages(messages),
+            tools: {
+                // server-side tool with execute function:
+                makeUserAssigmentCompleted: {
+                    description:
+                        'Function to mark the assignment as completed, you must only call it when the student code is correct and working properly satisfying the requirements of the assignment.',
+                    parameters: z.object({
+                        assignmentId: z.string().describe('The ID of the assignment to mark as completed.')
+                    }),
+                    execute: async function ({ assignmentId }) {
+                        // Mark the assignment as completed
+                        const userData = await supabase.auth.getUser()
+                        if (userData.error) {
+                            console.log('Error getting user data', userData.error)
+                        } else {
+                            const task = await supabase.from('lesson_completions').insert({
+                                lesson_id: lessonId,
+                                user_id: userData.data.user.id
+                            })
 
-# get_to_know_me.py
+                            if (task.error) {
+                                console.log(task.error)
+                                return {
+                                    status: 'error',
+                                    message: 'Error marking the assignment as completed.'
+                                }
+                            }
+                        }
 
-# Ask the user for their name
-
-name = input("What is your name? ")
-
-# Ask the user for their age
-
-age = int(input("How old are you? "))
-
-# Ask the user for their favorite color
-
-favorite_color = input("What is your favorite color? ")
-
-# Print out the user's information
-
-print(f"My name is {name}, I am {age} years old, and my favorite color is {favorite_color}.")
-
-\`\`\`
-
-
-If the student has a code satisfying the requirements of the assignment and working properly, YOU MUST MARK THE ASSIGNMENT AS COMPLETED. to do that you need to call the function \`makeUserAssigmentCompleted\` that way the student assignment will be marked as completed.
-DO NOT CALL THE FUNCTION IF THE STUDENT CODE IS NOT WORKING PROPERLY.
-        `,
-        tools: {
-            // server-side tool with execute function:
-            makeUserAssigmentCompleted: {
-                description:
-        'Function to mark the assignment as completed, you must only call it when the student code is correct and working properly satisfying the requirements of the assignment.',
-                parameters: z.object({
-                    assignmentId: z.string().describe('The ID of the assignment to mark as completed.')
-                }),
-                execute: async function ({ assignmentId }) {
-                    return {
-                        status: 'success',
-                        message: 'Assignment marked as completed.'
+                        return {
+                            status: 'success',
+                            message: 'Assignment marked as completed.'
+                        }
                     }
                 }
             }
-        }
-    })
+        })
 
-    // Respond with the stream
-    return result.toAIStreamResponse()
+        // Respond with the stream
+        return result.toAIStreamResponse()
+    } catch (error) {
+        console.error(error)
+        return new Response('Error', { status: 500 })
+    }
 }
