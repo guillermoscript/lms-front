44e9aaef6ddbbd17ed760502994a7b694a84fb1b
guillermoscript <arepayquezo@gmail.com>
2024-06-07 23:02:46 -0400

    feat: Add PasswordComponent for handling password input

---
+++ components/auth/PasswordComponent.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { EyeIcon, EyeOffIcon } from 'lucide-react'
+import { useState } from 'react'
+
+export default function PasswordComponent () {
+    const [showPassword, setShowPassword] = useState(false)
+
+    return (
+        <div className="rounded-md px-4 py-2 w-full bg-inherit border mb-6 gap-4 flex items-center justify-between">
+            <input
+                className="border-none w-full"
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                placeholder="••••••••"
+                required
+            />
+            <button
+                className=''
+                onClick={() => setShowPassword(!showPassword)}
+            >
+                {!showPassword ? (
+                    <EyeIcon />
+                ) : (
+                    <EyeOffIcon />
+                )}
+            </button>
+        </div>
+    )
+}
