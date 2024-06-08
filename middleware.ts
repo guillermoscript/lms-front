6e469a7c6c793ef5f2df18667dccfc88ce551edf
guillermoscript <arepayquezo@gmail.com>
2024-06-08 15:57:06 -0400

    refactor: Update middleware.ts and client.ts for code formatting consistency
    This commit updates the middleware.ts and client.ts files to improve code formatting consistency. It fixes indentation, adds missing semicolons, and removes unnecessary whitespace. This change ensures that the codebase follows consistent coding standards, making it easier to read and maintain.

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import { type NextRequest } from 'next/server'
+
 import { updateSession } from '@/utils/supabase/middleware'
 
-export async function middleware(request: NextRequest) {
-  // update user's auth session
-  return await updateSession(request)
+export async function middleware (request: NextRequest) {
+    // update user's auth session
+    return await updateSession(request)
 }
 
 export const config = {
-  matcher: [
+    matcher: [
     /*
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
@@ -15,6 +16,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+    ]
 }
--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,9 @@
 import { createBrowserClient } from '@supabase/ssr'
+
 import { Database } from './supabase'
 
 export const createClient = () =>
-  createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+    createBrowserClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
