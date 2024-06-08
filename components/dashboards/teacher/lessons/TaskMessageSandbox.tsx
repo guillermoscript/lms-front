0766f399d94d4794f16eca1e6be0f4d8684ec753
guillermoscript <arepayquezo@gmail.com>
2024-06-07 22:38:50 -0400

    feat: Add TaskMessageSandbox component for teacher lessons
    This commit adds the TaskMessageSandbox component to the codebase, which is responsible for handling chat logic and rendering chat messages in the teacher lessons dashboard. It includes functionality for sending and receiving messages, error handling, and tool invocations. This addition enhances the teacher's ability to communicate with students during lessons and improves the overall chat experience.

---
+++ components/dashboards/teacher/lessons/TaskMessageSandbox.tsx
@@ -0,0 +1,76 @@
+'use client'
+import { nanoid } from 'ai'
+import { useChat } from 'ai/react'
+import { useState } from 'react'
+
+import {
+    ChatInput,
+    ChatWindow
+} from '@/components/dashboards/Common/chat/chat'
+import { useToast } from '@/components/ui/use-toast'
+
+// Custom Hook for Chat logic
+function useChatLogic (systemPrompt: string) {
+    const { toast } = useToast()
+    const [isLessonAiTaskCompleted, setIsLessonAiTaskCompleted] = useState<boolean>(false)
+
+    const { messages, input, handleInputChange, stop, append, isLoading } =
+    useChat({
+        api: '/api/lessons/chat/teacher',
+        maxAutomaticRoundtrips: 5,
+        initialMessages: [
+            { role: 'assistant', content: systemPrompt, id: nanoid() }
+        ],
+        onError (error) {
+            console.log(error)
+            toast({
+                title: 'Error',
+                description: error.message,
+                variant: 'destructive'
+            })
+        },
+        async onToolCall ({ toolCall }) {
+            if (toolCall.toolName === 'makeUserAssigmentCompleted') {
+                setIsLessonAiTaskCompleted(true)
+            }
+        }
+    })
+
+    return {
+        messages,
+        input,
+        handleInputChange,
+        stop,
+        append,
+        isLoading,
+        isLessonAiTaskCompleted
+    }
+}
+
+// Main Component
+export default function TaskMessageSandbox ({
+    systemPrompt
+}: {
+    systemPrompt: string
+}) {
+    const { messages, stop, append, isLoading, isLessonAiTaskCompleted } = useChatLogic(
+        systemPrompt
+    )
+
+    return (
+        <>
+            <div className="flex flex-col gap-4 rounded border p-4">
+                <ChatWindow messages={messages} />
+            </div>
+            {!isLessonAiTaskCompleted && (
+                <ChatInput
+                    isLoading={isLoading}
+                    stop={stop}
+                    callbackFunction={async (message) => {
+                        append(message)
+                    }}
+                />
+            )}
+        </>
+    )
+}
