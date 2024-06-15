8180a9c27b6524e4ff7643f3ddbc4cd89a2ff450
guillermoscript <arepayquezo@gmail.com>
2024-06-15 16:14:21 -0400

    refactor: Remove shadow-md from CoursesStudentPage grid
    This commit removes the shadow-md class from the grid container in the CoursesStudentPage component. The shadow-md class was unnecessary and caused visual inconsistencies. This change improves the overall layout and visual consistency of the page.

--- app/dashboard/student/page.tsx
+++ app/dashboard/student/page.tsx
@@ -64,7 +64,7 @@ export default async function CoursesStudentPage () {
                     <h2 className="text-xl font-semibold text-primary-500 dark:text-primary-400">
                    Your Courses
                     </h2>
-                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 shadow-md">
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                         <AllCoursesCard
                             userId={user.data.user.id}
                             supabase={supabase}
@@ -76,7 +76,7 @@ export default async function CoursesStudentPage () {
                     <h2 className="text-xl font-semibold text-primary-500 dark:text-primary-400">
                     Your Courses
                     </h2>
-                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 shadow-md">
+                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                         {userCourses.data.map((course) => {
                             return (
                                 <>
