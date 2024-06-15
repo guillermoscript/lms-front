986085e40578d6dde30f673b4a0dfbe7bdf0d504
guillermoscript <arepayquezo@gmail.com>
2024-06-15 16:14:36 -0400

    refactor: Update OrdersPage to include transaction details
    This commit modifies the OrdersPage component in the app/dashboard/account/orders/columns.tsx file. It updates the column definitions to include transaction details such as product name, plan ID, amount, transaction date, payment method, status, currency, and actions. This change improves the accuracy and presentation of the order information on the page.

--- app/dashboard/account/orders/columns.tsx
+++ app/dashboard/account/orders/columns.tsx
@@ -2,7 +2,7 @@
 'use client'
 
 import { ColumnDef } from '@tanstack/react-table'
-import { MoreHorizontal } from 'lucide-react'
+import { CheckCircleIcon, ClockIcon, MoreHorizontal } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -14,23 +14,24 @@ import {
     DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { DataTableColumnHeader } from '@/components/ui/Table/DataTableColumnHeader'
-import { Database, Tables } from '@/utils/supabase/supabase'
+import { Tables } from '@/utils/supabase/supabase'
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-export interface Order {
+export interface TransactionCols {
     id: Tables<'transactions'>
-    // amount: number;
-    currencies: Database['public']['Tables']['currencies']['Row']['code']
-    status: Tables<'transactions'>
-    created_at: Tables<'transactions'>
-    paid_at: Tables<'transactions'>
-    due_date: Tables<'transactions'>
-    actions: string | JSX.Element
+    product_name: string
+    plan_id: number
+    amount: number
+    transaction_date: string
+    payment_method: string
+    status: string
+    currency: string
+    actions: React.ReactNode
 }
 
-export const orderCols: Array<ColumnDef<Order>> = [
+export const orderCols: Array<ColumnDef<TransactionCols>> = [
     {
         accessorKey: 'id',
         header: ({ column }) => (
@@ -41,64 +42,69 @@ export const orderCols: Array<ColumnDef<Order>> = [
         accessorKey: 'status',
         header: ({ column }) => (
             <DataTableColumnHeader column={column} title="Status" />
-        )
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex items-center">
+                    {row.original.status === 'successful' ? (
+                        <CheckCircleIcon className="h-5 w-5 text-green-500" />
+                    ) : row.original.status === 'pending' ? (
+                        <ClockIcon className="h-5 w-5 text-yellow-500" />
+                    ) : null}
+                    <span className="ml-2">{row.original.status}</span>
+                </div>
+            )
+        }
     },
     {
-        accessorKey: 'created_at',
+        accessorKey: 'product_name',
         header: ({ column }) => (
-            <DataTableColumnHeader column={column} title="Created At" />
+            <DataTableColumnHeader column={column} title="Product Name" />
         )
     },
     {
-        accessorKey: 'paid_at',
+        accessorKey: 'amount',
         header: ({ column }) => (
-            <DataTableColumnHeader column={column} title="Paid At" />
+            <DataTableColumnHeader column={column} title="Amount" />
         )
     },
     {
-        accessorKey: 'due_date',
+        accessorKey: 'transaction_date',
         header: ({ column }) => (
-            <DataTableColumnHeader column={column} title="Due Date" />
+            <DataTableColumnHeader column={column} title="Transaction Date" />
         )
     },
     {
-        accessorKey: 'currencies.code',
+        accessorKey: 'payment_method',
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Payment Method" />
+        )
+    },
+    {
+        accessorKey: 'currency',
         header: ({ column }) => (
             <DataTableColumnHeader column={column} title="Currency" />
         )
     },
-    // {
-    // 	accessorKey: "amount",
-    // 	header: "Amount",
-    // },
     {
-        id: 'actions',
+        accessorKey: 'actions',
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Actions" />
+        ),
         cell: ({ row }) => {
-            const payment = row.original
-            console.log(row)
-
             return (
                 <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
+                    <DropdownMenuTrigger>
+                        <Button>
+                            <MoreHorizontal />
                         </Button>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
+                    <DropdownMenuContent>
                         <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        {/* <DropdownMenuItem
-							onClick={() =>
-								navigator.clipboard.writeText(payment.id)
-							}
-						>
-							Copy payment ID
-						</DropdownMenuItem> */}
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>
-                      View payment details
-                        </DropdownMenuItem>
+                        <DropdownMenuItem>View</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             )
