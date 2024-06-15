fd78d74a2a63b8120c4b9e88994083c408efad0a
guillermoscript <arepayquezo@gmail.com>
2024-06-15 13:50:49 -0400

    ```text refactor: Update CoursesPageLayout to use EnrollCard component
    This commit refactors the CoursesPageLayout component in the student/courses/[courseId]/layout.tsx file. It replaces the previous implementation of displaying the enrollment card with the EnrollCard component. This change improves code readability and maintainability by using a dedicated component for course enrollment.

--- app/dashboard/student/courses/[courseId]/layout.tsx
+++ app/dashboard/student/courses/[courseId]/layout.tsx
@@ -1,7 +1,5 @@
 
-import EnrollButton from '@/components/dashboards/student/course/EnrollButton'
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import EnrollCard from '@/components/dashboards/student/course/EnrollCard'
 import { createClient } from '@/utils/supabase/server'
 
 export default async function CoursesPageLayout ({
@@ -23,56 +21,21 @@ export default async function CoursesPageLayout ({
         .from('enrollments')
         .select('enrollment_id')
         .eq('user_id', user.data.user.id)
-        .eq('course_id', params.courseId)
+        .eq('course_id', Number(params.courseId))
 
     if (isUserEnrolled.error != null) {
         if (isUserEnrolled.error.code === 'PGRST116') {
             return (
-                <div className='grid gap-8'>
-                    <Breadcrumb>
-                        <BreadcrumbList>
-                            <BreadcrumbItem>
-                                <BreadcrumbLink href="/dashboard">
-                          Dashboard
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator />
-                            <BreadcrumbItem>
-                                <BreadcrumbLink
-                                    className="text-primary-500 dark:text-primary-400"
-                                    href="/dashboard/student"
-                                >
-                            Student
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator />
-                            <BreadcrumbItem>
-                                <BreadcrumbLink
-                                    className="text-primary-500 dark:text-primary-400"
-                                    href="/dashboard/student/courses"
-                                >
-                            Courses
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>
-                                        You are not enrolled in this course
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <p className=' mb-4'>
-                                        You need to enroll in this course to access the lessons.
-                            </p>
-                            <EnrollButton courseId={Number(params.courseId)} />
-                        </CardContent>
-                    </Card>
-                </div>
+                <EnrollCard courseId={Number(params.courseId)} />
             )
         }
         throw new Error(isUserEnrolled.error.message)
+    }
+
+    if (isUserEnrolled.data.length === 0) {
+        return (
+            <EnrollCard courseId={Number(params.courseId)} />
+        )
     }
 
     return (
