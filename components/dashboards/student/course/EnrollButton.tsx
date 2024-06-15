ac4eb739085bdaabd5a3dc2eb0af2e4b3e27be20
guillermoscript <arepayquezo@gmail.com>
2024-06-15 13:41:31 -0400

    refactor: Add EnrollButton component for course enrollment
    This commit adds the EnrollButton component to the student course dashboard. The EnrollButton component allows users to enroll in a course by calling the enrollUserToCourseAction function. It handles the loading state and displays appropriate toast messages based on the enrollment status. This change enhances the user experience by providing a convenient way for students to enroll in courses.

---
+++ components/dashboards/student/course/EnrollButton.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useState } from 'react'
+
+import { enrollUserToCourseAction } from '@/actions/dashboard/courseActions'
+import { Button } from '@/components/ui/button'
+import { useToast } from '@/components/ui/use-toast'
+
+export default function EnrollButton ({
+    courseId
+}: {
+    courseId: number
+}) {
+    const { toast } = useToast()
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <Button
+            onClick={ async () => {
+                setLoading(true)
+                try {
+                    const enrollUser = await enrollUserToCourseAction({
+                        courseId
+                    })
+
+                    console.log(enrollUser)
+
+                    if (enrollUser.status === 'error') {
+                        return toast({
+                            title: enrollUser.message,
+                            description: enrollUser.error,
+                            variant: 'destructive'
+                        })
+                    }
+
+                    toast({
+                        title: enrollUser.message
+                    })
+                } catch (error) {
+                    toast({
+                        title: 'Error enrolling user',
+                        description: error.message,
+                        variant: 'destructive'
+                    })
+                } finally {
+                    setLoading(false)
+                }
+            }}
+        >
+            {loading ? 'Enrolling...' : 'Enroll Now'}
+        </Button>
+    )
+}
