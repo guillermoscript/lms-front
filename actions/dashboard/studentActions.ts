a5da31f2b9d433c376e3897566a451fc6074ee49
guillermoscript <arepayquezo@gmail.com>
2024-06-16 15:26:01 -0400

    feat: Add updateUserProfile function to update user profile information
    This commit adds the updateUserProfile function to the actions/dashboard/studentActions.ts file. The function allows users to update their profile information, including their full name, bio, and avatar URL. It uses the Supabase client to interact with the database and performs validation to handle potential errors. This addition enhances the student account functionality by providing an easy way for users to update their profile information.

--- actions/dashboard/studentActions.ts
+++ actions/dashboard/studentActions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from 'next/cache'
 
+import { updateUserProfileSchema } from '@/components/dashboards/student/account/EditProfileForm'
 import { createResponse } from '@/utils/functions'
 import { createClient } from '@/utils/supabase/server'
 
@@ -59,4 +60,33 @@ export async function cancelSubscription ({
 
     revalidatePath('/dashboard/student/account/subscriptions', 'layout')
     return createResponse('success', 'Subscription cancelled successfully', null, null)
+}
+
+export async function updateUserProfile ({
+    fullName,
+    bio,
+    avatarUrl
+}: updateUserProfileSchema) {
+    const supabase = createClient()
+    const userData = await supabase.auth.getUser()
+
+    if (userData.error) {
+        return createResponse('error', 'Error updating profile', null, 'Error updating profile')
+    }
+
+    const profileUpdate = await supabase
+        .from('profiles')
+        .update({
+            full_name: fullName,
+            bio,
+            avatar_url: avatarUrl
+        })
+        .eq('id', userData.data.user?.id)
+
+    if (profileUpdate.error) {
+        return createResponse('error', 'Error updating profile', null, 'Error updating profile')
+    }
+
+    revalidatePath('/dashboard/student/account/profile', 'layout')
+    return createResponse('success', 'Profile updated successfully', null, null)
 }
