f8b2264444e38f94a99150f6ca348cebef5c2cbe
guillermoscript <arepayquezo@gmail.com>
2024-06-16 20:01:18 -0400

    refactor: Update CommentsSections component with improved UI and functionality
    This commit updates the CommentsSections component in the components/dashboards/Common/CommentsSections.tsx file. The changes include refactoring the component to improve its UI and functionality. It removes unused imports, simplifies the code structure, and adds support for lazy loading of images. These updates enhance the user experience by improving the performance and visual appeal of the comments section in the dashboard.

--- components/dashboards/Common/CommentsSections.tsx
+++ components/dashboards/Common/CommentsSections.tsx
@@ -1,30 +1,18 @@
 import dayjs from 'dayjs'
-import { User2 } from 'lucide-react'
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle
-} from '@/components/ui/card'
-import ViewMarkdown from '@/components/ui/markdown/ViewMarkdown'
-import { Separator } from '@/components/ui/separator'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { createClient } from '@/utils/supabase/server'
-import { Tables } from '@/utils/supabase/supabase'
 
 import CommentEditor from '../student/course/lessons/CommentEditor'
+import CommentCard from './comments/CommentsCard'
 
 export default async function CommentsSections ({
     lesson_id,
     lesson_comments
-}: {
-    lesson_id: number
-    lesson_comments: Array<Tables<'lesson_comments'>>
 }) {
     const supabase = createClient()
     const usersIds = lesson_comments.map((comment) => comment.user_id)
+    const currentUser = await supabase.auth.getUser()
     const profiles = await supabase
         .from('profiles')
         .select('*')
@@ -36,23 +24,35 @@ export default async function CommentsSections ({
     }
 
     return (
-        <div className="flex flex-col gap-4">
-            {lesson_comments?.map((comment) => (
-                <CommentCard
-                    key={comment.id}
-                    name={profiles.data[comment.user_id]?.full_name || 'Unknown'}
-                    comment={comment.content}
-                    date={dayjs(comment.created_at).format('DD/MM/YYYY: HH:mm')}
-                />
-            ))}
-            <Card>
-                <CardHeader>
-                    <Avatar>
-                        <AvatarImage src="/img/favicon.png" alt="profile" />
-                        <AvatarFallback>
-                            <User2 className="h-6 w-6" />
-                        </AvatarFallback>
-                    </Avatar>
+        <div className="flex flex-col gap-4 p-4">
+            <h2 className="text-xl font-semibold mb-4">Comments</h2>
+            {lesson_comments
+                .filter(comment => comment.parent_comment_id === null)
+                .map((comment) => {
+                    const user = profiles.data.find((profile) => profile.id === comment.user_id)
+                    const isReactionPresent = comment.comment_reactions.find(
+                        (reaction) => reaction.user_id === currentUser.data.user.id
+                    )
+
+                    return (
+                        <CommentCard
+                            key={comment.id}
+                            comment={comment}
+                            name={user?.full_name || 'Unknown'}
+                            avatar={user?.avatar_url || '/img/favicon.png'}
+                            isAuthor={comment.user_id === currentUser.data.user.id}
+                            comment_reactions={comment.comment_reactions}
+                            isReactionPresent={isReactionPresent}
+                            date={dayjs(comment.created_at).format('DD/MM/YYYY: HH:mm')}
+                            allComments={lesson_comments}
+                            profiles={profiles.data} // Pass profiles data here
+                            currentUser={currentUser.data.user}
+                        />
+                    )
+                })}
+            <Card className="mt-6">
+                <CardHeader className="flex items-center gap-2">
+
                     <CardTitle className="text-lg font-medium">Add a Comment</CardTitle>
                 </CardHeader>
                 <CardContent>
@@ -60,39 +60,5 @@ export default async function CommentsSections ({
                 </CardContent>
             </Card>
         </div>
-    )
-}
-
-const CommentCard = ({
-    name,
-    comment,
-    date
-}: {
-    name: string
-    comment: string
-    date: string
-}) => {
-    return (
-        <Card>
-            <CardHeader className="flex p-2 flex-row flex-wrap items-baseline gap-2">
-                <Avatar>
-                    <AvatarImage src="/img/favicon.png" alt="profile" />
-                    <AvatarFallback>{name[0]}</AvatarFallback>
-                </Avatar>
-                <CardTitle className="text-lg font-medium">{name}</CardTitle>
-                <CardDescription className="text-xs text-gray-400">
-                    {date}
-                </CardDescription>
-            </CardHeader>
-            <Separator />
-            <CardContent
-                className='p-4'
-            >
-                <div className="flex flex-col">
-                    <div className="flex items-center gap-2"></div>
-                    <ViewMarkdown markdown={comment} />
-                </div>
-            </CardContent>
-        </Card>
     )
 }
