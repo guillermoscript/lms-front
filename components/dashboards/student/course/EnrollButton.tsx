e6676277b121d45be3915ed23742d690a4d1b7e9
guillermoscript <arepayquezo@gmail.com>
2024-06-22 11:13:33 -0400

    refactor: Update CourseCard component for improved UI and functionality

--- components/dashboards/student/course/EnrollButton.tsx
+++ components/dashboards/student/course/EnrollButton.tsx
@@ -6,55 +6,44 @@ import { enrollUserToCourseAction } from '@/actions/dashboard/courseActions'
 import { Button } from '@/components/ui/button'
 import { useToast } from '@/components/ui/use-toast'
 
-export default function EnrollButton ({
-    courseId
-}: {
-    courseId: number
-}) {
+export default function EnrollButton ({ courseId }: { courseId: number }) {
     const { toast } = useToast()
     const [loading, setLoading] = useState(false)
 
     return (
-        <form
-            onSubmit={async (e) => {
-                e.preventDefault()
-                async () => {
-                    setLoading(true)
-                    try {
-                        const enrollUser = await enrollUserToCourseAction({
-                            courseId
-                        })
+        <Button
+            disabled={loading}
+            onClick={async () => {
+                setLoading(true)
+                try {
+                    const enrollUser = await enrollUserToCourseAction({
+                        courseId
+                    })
 
-                        console.log(enrollUser)
-
-                        if (enrollUser.status === 'error') {
-                            return toast({
-                                title: enrollUser.message,
-                                description: enrollUser.error,
-                                variant: 'destructive'
-                            })
-                        }
-
-                        toast({
-                            title: enrollUser.message
-                        })
-                    } catch (error) {
-                        toast({
-                            title: 'Error enrolling user',
-                            description: error.message,
+                    if (enrollUser.status === 'error') {
+                        return toast({
+                            title: enrollUser.message,
+                            description: enrollUser.error,
                             variant: 'destructive'
                         })
-                    } finally {
-                        setLoading(false)
                     }
+
+                    toast({
+                        title: enrollUser.message
+                    })
+                } catch (error) {
+                    toast({
+                        title: 'Error enrolling user',
+                        description: error.message,
+                        variant: 'destructive'
+                    })
+                } finally {
+                    setLoading(false)
                 }
-            }}
+            }
+            }
         >
-            <Button
-                disabled={loading}
-            >
-                {loading ? 'Enrolling...' : 'Enroll Now'}
-            </Button>
-        </form>
+            {loading ? 'Enrolling...' : 'Enroll Now'}
+        </Button>
     )
 }
