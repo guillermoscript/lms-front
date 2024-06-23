47d1ffdb49730de17cdcbbd9d7cd0228d8e853ca
guillermoscript <arepayquezo@gmail.com>
2024-06-23 16:12:34 -0400

    wip

--- actions/dashboard/linkProductToCourse.ts
+++ actions/dashboard/linkProductToCourse.ts
@@ -4,9 +4,9 @@ import { createResponse } from '@/utils/functions'
 export async function linkProductAction (data: courseSchemaType) {
     console.log(data)
 
-    if (!data.price || !data.status || !data.course_id) {
-        return createResponse('error', 'Please fill in all fields', null, null)
-    }
+    // if (!data.price || !data.status || !data.course_id) {
+    //     return createResponse('error', 'Please fill in all fields', null, null)
+    // }
 
     return createResponse('success', 'Product linked successfully', null, null)
 }
