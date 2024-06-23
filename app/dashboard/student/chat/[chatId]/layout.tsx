9d4f12dae2bc20196ddd3687d23b54319ee5e5dc
guillermoscript <arepayquezo@gmail.com>
2024-06-23 15:40:05 -0400

    feat: Add CoursesLayout component for chat page
    This commit adds the CoursesLayout component, which provides the layout for the student chat page. It fetches chat data from the Supabase database based on the chat ID and renders the chat title along with the children components. This enhancement improves the organization and structure of the chat page, enhancing the user experience.

---
+++ app/dashboard/student/chat/[chatId]/layout.tsx
@@ -0,0 +1,33 @@
+import { createClient } from '@/utils/supabase/server'
+
+export default async function CoursesLayout ({
+    children,
+    params
+}: {
+    children: React.ReactNode
+    params: {
+        chatId: string
+    }
+}) {
+    const supabase = createClient()
+
+    const chatData = await supabase
+        .from('chats')
+        .select('*')
+        .eq('chat_id', Number(params.chatId))
+        .single()
+
+    if (chatData.error) {
+        console.log(chatData.error)
+        throw new Error('Error fetching chat')
+    }
+
+    return (
+        <div className='flex flex-col gap-4 p-4'>
+            <h1 className="text-2xl font-semibold text-gray-800">
+                {chatData.data.title}
+            </h1>
+            {children}
+        </div>
+    )
+}
