5ba9491931d1cb2c70b69ba3fc6675268f7f78ed
guillermoscript <arepayquezo@gmail.com>
2024-06-25 23:00:52 -0400

    refactor: Update title and description in layout.tsx
    This commit updates the title and description in the layout.tsx file to reflect the changes made in the application. The title is changed to 'Next.js and Supabase AI powered LMS' and the description is changed to 'The fastest way to learn about programming'. This refactor improves the clarity and relevance of the application's metadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
     metadataBase: new URL(defaultUrl),
-    title: 'Next.js and Supabase Starter Kit',
-    description: 'The fastest way to build apps with Next.js and Supabase'
+    title: 'Next.js and Supabase AI powered LMS',
+    description: 'The fastest way to learn about programming'
 }
 
 export default function RootLayout ({
