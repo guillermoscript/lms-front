7fd8821401653a75d472296c21e6a3faeb4ff101
guillermoscript <arepayquezo@gmail.com>
2024-07-03 21:19:33 -0400

    refactor: Update student chat actions and components
    This commit updates the student chat actions and components. It introduces the `studentInsertChatMessage` function to handle the submission of chat messages by students. It also adds the `studentUpdateChat` function to allow students to update the title of a chat. These changes improve the functionality and user experience of the chat feature for students.

--- actions/dashboard/chatActions.ts
+++ actions/dashboard/chatActions.ts
@@ -33,6 +33,54 @@ export async function studentCreateNewChat (state: {
     return createResponse('success', 'Chat created successfully', chatInsert.data, null)
 }
 
+export async function studentInsertChatMessage (state: {
+    chatId: number
+    message: string
+}) {
+    const supabase = createClient()
+    const userData = await supabase.auth.getUser()
+
+    if (userData.error) {
+        return createResponse('error', 'Error no user found', null, 'Error no user found')
+    }
+
+    const messageInsert = await supabase.from('messages').insert({
+        chat_id: state.chatId,
+        message: state.message,
+        sender: 'user',
+        created_at: new Date().toISOString()
+    })
+
+    if (messageInsert.error) {
+        return createResponse('error', 'Error creating message', null, 'Error creating message')
+    }
+
+    return createResponse('success', 'Message sent successfully', null, null)
+}
+
+export async function studentUpdateChat (state: {
+    chatId: number
+    title: string
+}) {
+    const supabase = createClient()
+    const userData = await supabase.auth.getUser()
+
+    if (userData.error) {
+        return createResponse('error', 'Error no user found', null, 'Error no user found')
+    }
+
+    const chatUpdate = await supabase.from('chats').update({
+        title: state.title
+    }).eq('chat_id', state.chatId)
+
+    if (chatUpdate.error) {
+        return createResponse('error', 'Error updating chat', null, 'Error updating chat')
+    }
+
+    // revalidatePath('/dashboard/student/chat/', 'layout')
+    return createResponse('success', 'Chat updated successfully', null, null)
+}
+
 export async function studentSubmitMessage (state: {
     chatId: number
     message: string
@@ -44,16 +92,15 @@ export async function studentSubmitMessage (state: {
         return createResponse('error', 'Error no user found', null, 'Error no user found')
     }
 
-    const studentId = userData.data.user?.id
-
     const messageInsert = await supabase.from('messages').insert({
-        user_id: studentId,
         chat_id: state.chatId,
-        content: state.message,
+        message: state.message,
+        sender: 'user',
         created_at: new Date().toISOString()
     })
 
     if (messageInsert.error) {
+        console.log(messageInsert.error)
         return createResponse('error', 'Error creating message', null, 'Error creating message')
     }
 
