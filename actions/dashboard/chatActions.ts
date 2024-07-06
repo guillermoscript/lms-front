9db4222ce8c153b907b92094a45c279c09c0f22d
guillermoscript <arepayquezo@gmail.com>
2024-07-06 12:46:03 -0400

    refactor: Add studentUpdateChatTitle function for updating chat title

--- actions/dashboard/chatActions.ts
+++ actions/dashboard/chatActions.ts
@@ -58,6 +58,55 @@ export async function studentInsertChatMessage (state: {
     return createResponse('success', 'Message sent successfully', null, null)
 }
 
+export async function studentUpdateChatTitle (state: {
+    chatId: number
+    title: string
+}) {
+    const supabase = createClient()
+    const userData = await supabase.auth.getUser()
+
+    if (userData.error) {
+        return createResponse('error', 'Error no user found', null, 'Error no user found')
+    }
+
+    const chatUpdate = await supabase.from('chats').update({
+        title: state.title
+    }).eq('chat_id', state.chatId)
+
+    if (chatUpdate.error) {
+        return createResponse('error', 'Error updating chat', null, 'Error updating chat')
+    }
+
+    // revalidatePath('/dashboard/student/chat/', 'layout')
+    return createResponse('success', 'Chat updated successfully', null, null)
+}
+
+export async function insertChatMessage (state: {
+    chatId: number
+    message: string
+    sender: Tables<'messages'>['sender']
+}) {
+    const supabase = createClient()
+    const userData = await supabase.auth.getUser()
+
+    if (userData.error) {
+        return createResponse('error', 'Error no user found', null, 'Error no user found')
+    }
+
+    const messageInsert = await supabase.from('messages').insert({
+        chat_id: state.chatId,
+        message: state.message,
+        sender: state.sender,
+        created_at: new Date().toISOString()
+    })
+
+    if (messageInsert.error) {
+        return createResponse('error', 'Error creating message', null, 'Error creating message')
+    }
+
+    return createResponse('success', 'Message sent successfully', null, null)
+}
+
 export async function studentUpdateChat (state: {
     chatId: number
     title: string
