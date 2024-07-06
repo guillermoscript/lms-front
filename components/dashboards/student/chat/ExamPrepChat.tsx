2d80c446af1d4aa6d5fa2d555fa7cda7ab757658
guillermoscript <arepayquezo@gmail.com>
2024-07-06 12:46:19 -0400

    refactor: Add ChatLoadingSkeleton component and update student chat actions
    This commit adds the ChatLoadingSkeleton component to the student chat feature, improving the user experience by displaying a loading skeleton when the chat is loading. It also updates the student chat actions by introducing the studentInsertChatMessage function to handle the submission of chat messages by students. These changes enhance the functionality and usability of the chat feature for students.

--- components/dashboards/student/chat/ExamPrepChat.tsx
+++ components/dashboards/student/chat/ExamPrepChat.tsx
@@ -3,20 +3,18 @@ import { generateId } from 'ai'
 import { useActions, useAIState, useUIState } from 'ai/rsc'
 import { useEffect, useState } from 'react'
 
-import { studentInsertChatMessage } from '@/actions/dashboard/chatActions'
+import { studentInsertChatMessage, studentUpdateChatTitle } from '@/actions/dashboard/chatActions'
 import { AI, ClientMessage, UIState } from '@/actions/dashboard/ExamPreparationActions'
 import ViewMarkdown from '@/components/ui/markdown/ViewMarkdown'
-import { Skeleton } from '@/components/ui/skeleton'
 
 import { ChatInput } from '../../Common/chat/chat'
 import Message from '../../Common/chat/Message'
 import SuggestionsContainer from '../../Common/chat/SuggestionsContainer'
+import ChatLoadingSkeleton from './ChatLoadingSkeleton'
 
 export default function ExamPrepChat ({
-    children,
     chatId
 }: {
-    children?: React.ReactNode
     chatId?: number
 }) {
     const [conversation, setConversation] = useUIState<typeof AI>()
@@ -94,25 +92,20 @@ export default function ExamPrepChat ({
                 {conversation.length ? (
                     <ChatList messages={conversation} />
                 ) : (
-                    <div className="flex-1 flex items-center justify-center">
-                        <p className="text-gray-400">No messages yet</p>
+                    <div className="flex-1 flex items-center justify-center my-4">
+                        <p className="text-gray-400">
+                            Please write a message with the examn you want, like "help me creating a basic
+                            <span
+                                className="text-gray-600 font-semibold"
+                            >
+                            [Exam Subject]
+                            </span> examn form for me please"
+                        </p>
                     </div>
                 )}
 
                 {isLoading && (
-                    <div className="space-y-2 w-full">
-                        <Skeleton className="h-6 rounded mr-14" />
-                        <div className="grid grid-cols-3 gap-4">
-                            <Skeleton className="h-6 rounded col-span-2" />
-                            <Skeleton className="h-6 rounded col-span-1" />
-                        </div>
-                        <div className="grid grid-cols-4 gap-4">
-                            <Skeleton className="h-6 rounded col-span-1" />
-                            <Skeleton className="h-6 rounded col-span-2" />
-                            <Skeleton className="h-6 rounded col-span-1 mr-4" />
-                        </div>
-                        <Skeleton className="h-6 rounded" />
-                    </div>
+                    <ChatLoadingSkeleton />
                 )}
             </div>
             {!hideInput && (
@@ -124,12 +117,14 @@ export default function ExamPrepChat ({
 
                         setIsLoading(true)
 
-                        if (chatId) {
-                            const inserUserMessage = await studentInsertChatMessage({
+                        if (aiState.messages.length === 0 && chatId) {
+                            console.log('First message')
+                            const message = await studentUpdateChatTitle({
                                 chatId,
-                                message: input.content
+                                title: input.content
                             })
-                            console.log(inserUserMessage)
+
+                            console.log(message)
                         }
 
                         setConversation((currentConversation: ClientMessage[]) => [
@@ -148,6 +143,14 @@ export default function ExamPrepChat ({
                                 )
                             }
                         ])
+
+                        if (chatId) {
+                            const inserUserMessage = await studentInsertChatMessage({
+                                chatId,
+                                message: input.content
+                            })
+                            console.log(inserUserMessage)
+                        }
 
                         const message = await continueConversation(input.content)
 
