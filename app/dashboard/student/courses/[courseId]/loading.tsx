b8e929352e8681ef645a94f6cf79f4cebc41cd29
guillermoscript <arepayquezo@gmail.com>
2024-07-07 18:52:00 -0400

    feat: Add loading components for chat and courses
    This commit adds loading components for the chat and courses sections of the dashboard. The loading components display skeleton screens while the actual content is being fetched from the server. This addition improves the user experience by providing visual feedback and reducing perceived loading times.

---
+++ app/dashboard/student/courses/[courseId]/loading.tsx
@@ -0,0 +1,75 @@
+import ChatLoadingSkeleton from '@/components/dashboards/student/chat/ChatLoadingSkeleton'
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader
+} from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function Component () {
+    return (
+        <div className="flex flex-wrap gap-4 md:gap-6 items-center justify-center">
+            <Card>
+                <CardHeader>
+                    <Skeleton className="h-6 w-40 rounded-md" />
+                    <Skeleton className="h-8 w-20 rounded-md" />
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4 items-start justify-start min-w-[calc(30vw-2rem)]">
+                    <ChatLoadingSkeleton />
+                </CardContent>
+                <CardFooter>
+                    <Skeleton className="h-8 w-32 mx-auto" />
+                </CardFooter>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <Skeleton className="h-6 w-40 rounded-md" />
+                    <Skeleton className="h-8 w-20 rounded-md" />
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4 items-start justify-start min-w-[calc(30vw-2rem)]">
+                    <ChatLoadingSkeleton />
+                </CardContent>
+                <CardFooter>
+                    <Skeleton className="h-8 w-32 mx-auto" />
+                </CardFooter>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <Skeleton className="h-6 w-40 rounded-md" />
+                    <Skeleton className="h-8 w-20 rounded-md" />
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4 items-start justify-start min-w-[calc(30vw-2rem)]">
+                    <ChatLoadingSkeleton />
+                </CardContent>
+                <CardFooter>
+                    <Skeleton className="h-8 w-32 mx-auto" />
+                </CardFooter>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <Skeleton className="h-6 w-40 rounded-md" />
+                    <Skeleton className="h-8 w-20 rounded-md" />
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4 items-start justify-start min-w-[calc(30vw-2rem)]">
+                    <ChatLoadingSkeleton />
+                </CardContent>
+                <CardFooter>
+                    <Skeleton className="h-8 w-32 mx-auto" />
+                </CardFooter>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <Skeleton className="h-6 w-40 rounded-md" />
+                    <Skeleton className="h-8 w-20 rounded-md" />
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4 items-start justify-start min-w-[calc(30vw-2rem)]">
+                    <ChatLoadingSkeleton />
+                </CardContent>
+                <CardFooter>
+                    <Skeleton className="h-8 w-32 mx-auto" />
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
