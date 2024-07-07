a501cdda6e37033f7dc74cb611942b857de52873
guillermoscript <arepayquezo@gmail.com>
2024-07-07 16:51:03 -0400

    refactor: Update StudentChatSidebar to use Collapsible component
    This commit refactors the StudentChatSidebar component to use the newly added Collapsible component from the @radix-ui/react-collapsible library. The Collapsible component provides collapsible sections that can be expanded or collapsed, enhancing the UI functionality of the chat sidebar. This change improves the user experience and code maintainability.

--- components/dashboards/student/chat/StudentChatSidebar.tsx
+++ components/dashboards/student/chat/StudentChatSidebar.tsx
@@ -1,48 +1,20 @@
+import { CaretSortIcon } from '@radix-ui/react-icons'
 
 import ChatSidebarItem from '@/components/dashboards/student/chat/ChatSidebarItem'
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger
-} from '@/components/ui/accordion'
-import { ScrollArea } from '@/components/ui/scroll-area'
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger
+} from '@/components/ui/collapsible'
+import {
+    Command,
+    CommandEmpty,
+    CommandInput,
+    CommandItem,
+    CommandList
+} from '@/components/ui/command'
 import { createClient } from '@/utils/supabase/server'
 import { Tables } from '@/utils/supabase/supabase'
-
-const ChatAccordion = ({ chatType, userRole, chats }) => (
-    <Accordion className='w-full' type="single" collapsible>
-        <AccordionItem className='w-full' value={`item-${chatType}`}>
-            <AccordionTrigger
-                className='w-full text-lg font-semibold capitalize'
-            >{chatType.replace('_', ' ')}</AccordionTrigger>
-            <AccordionContent>
-                <ScrollArea className="h-[calc(100vh-4rem)]  w-full rounded-md border p-4">
-
-                    <ul className='w-full flex flex-col gap-3'>
-                        {chats.map((chat: Tables<'chats'>) => {
-                            const types = {
-                                free_chat: 'free-chat',
-                                qna: 'qa',
-                                exam_prep: 'exam-prep',
-                                course_chat: 'study-material'
-                            }
-
-                            return (
-                                <ChatSidebarItem
-                                    key={chat.chat_id}
-                                    chat={chat}
-                                    chatType={types[chatType]}
-                                    userRole={userRole}
-                                />
-                            )
-                        })}
-                    </ul>
-                </ScrollArea>
-            </AccordionContent>
-        </AccordionItem>
-    </Accordion>
-)
 
 export default async function StudentChatSidebar ({ userRole }) {
     const supabase = createClient()
@@ -60,15 +32,61 @@ export default async function StudentChatSidebar ({ userRole }) {
     }
 
     const chatTypes = {
-        free_chat: chats.data.filter(chat => chat.chat_type === 'free_chat'),
-        exam_prep: chats.data.filter(chat => chat.chat_type === 'exam_prep')
+        free_chat: chats.data.filter((chat) => chat.chat_type === 'free_chat'),
+        exam_prep: chats.data.filter((chat) => chat.chat_type === 'exam_prep')
     }
 
     return (
-        <nav className="flex flex-col gap-2 h-auto justify-start w-full border-none px-4 py-2 items-start bg-gray-100/40 dark:bg-gray-800/40">
-            {Object.entries(chatTypes).map(([type, chats]) => (
-                <ChatAccordion key={type} chatType={type} userRole={userRole} chats={chats} />
-            ))}
+        <nav className="flex flex-col gap-2 h-auto justify-start w-full border-none items-start ">
+            <Command
+                className='bg-gray-100/40 dark:bg-gray-800/40 border rounded-lg w-full'
+            >
+                <CommandInput placeholder="Type to search..." />
+                <CommandList className="w-full px-2 max-h-[calc(100vh-4rem)] overflow-y-auto">
+                    <CommandEmpty>No results found.</CommandEmpty>
+
+                    {Object.entries(chatTypes).map(([type, chats]) => {
+                        return (
+                            <Collapsible
+                                defaultOpen={true}
+                                className="w-full"
+                            >
+                                <CollapsibleTrigger
+                                    className="w-full text-md font-semibold capitalize flex items-center justify-between p-2 px-2 rounded-lg my-2 hover:bg-opacity-10 dark:hover:bg-opacity-10 hover:bg-gray-400 dark:hover:bg-gray-200"
+                                >
+                                    <p>
+                                        {type.replace('_', ' ')}
+                                    </p>
+                                    <CaretSortIcon className="h-4 w-4" />
+                                </CollapsibleTrigger>
+                                <CollapsibleContent
+                                    className='border-y border-gray-400 dark:border-gray-200'
+                                >
+                                    {chats.map((chat: Tables<'chats'>) => {
+                                        const types = {
+                                            free_chat: 'free-chat',
+                                            qna: 'qa',
+                                            exam_prep: 'exam-prep',
+                                            course_chat: 'study-material'
+                                        }
+
+                                        return (
+                                            <CommandItem value={chat.chat_id.toString()}>
+                                                <ChatSidebarItem
+                                                    key={chat.chat_id}
+                                                    chat={chat}
+                                                    chatType={types[type]}
+                                                    userRole={userRole}
+                                                />
+                                            </CommandItem>
+                                        )
+                                    })}
+                                </CollapsibleContent>
+                            </Collapsible>
+                        )
+                    })}
+                </CommandList>
+            </Command>
         </nav>
     )
 }
