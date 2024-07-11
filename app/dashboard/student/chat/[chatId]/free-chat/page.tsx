865924f104ae082d52eb621e8c6368f42a8680f7
guillermoscript <arepayquezo@gmail.com>
2024-07-10 22:25:47 -0400

    refactor: Use ChatLoadingSkeleton component in FreeChatPage
    This commit refactors the FreeChatPage component in the page.tsx file to use the ChatLoadingSkeleton component instead of manually rendering the loading UI. By abstracting the loading logic into a separate component, the code becomes more readable and maintainable. This change enhances the user experience by providing a consistent loading UI for the free chat page.

--- app/dashboard/student/chat/[chatId]/free-chat/page.tsx
+++ app/dashboard/student/chat/[chatId]/free-chat/page.tsx
@@ -1,5 +1,4 @@
 import FreeChat from '@/components/dashboards/student/chat/FreeChat'
-import { createClient } from '@/utils/supabase/server'
 
 export const dynamic = 'force-dynamic'
 export const maxDuration = 30
@@ -11,34 +10,12 @@ export default async function FreeChatPage ({
         chatId: string
     }
 }) {
-    const supabase = createClient()
-
-    const messagesData = await supabase
-        .from('messages')
-        .select('*')
-        .eq('chat_id', Number(params.chatId))
-        .order('created_at', { ascending: true })
-
-    if (messagesData.error) {
-        console.log(messagesData.error)
-        throw new Error('Error fetching messages')
-    }
-
     return (
 
         <>
-            <div className='flex flex-col gap-4 overflow-y-auto h-[calc(100vh)] md:h-[calc(100vh-4rem)]'>
-                <FreeChat
-                    chatId={Number(params.chatId)}
-                    initialMessages={
-                        messagesData.data.map(message => ({
-                            id: message.id.toString(),
-                            content: message.message,
-                            role: message.sender
-                        }))
-                    }
-                />
-            </div>
+            <FreeChat
+                chatId={Number(params.chatId)}
+            />
         </>
     )
 }
