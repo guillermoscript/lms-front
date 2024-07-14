339e6ba1a87bd4b5baaf485aaaf3fef77fd23043
guillermoscript <arepayquezo@gmail.com>
2024-07-14 14:46:08 -0400

    refactor: Update CourseCard component to improve code readability and maintainability

--- components/dashboards/student/course/CourseCard.tsx
+++ components/dashboards/student/course/CourseCard.tsx
@@ -7,12 +7,6 @@ import { cn } from '@/utils'
 
 const CourseCard = ({
     title,
-    progress,
-    totalLessons,
-    completedLessons,
-    completedTests,
-    totalTests,
-    approvedTests,
     courseId,
     children,
     img,
@@ -20,96 +14,36 @@ const CourseCard = ({
 }: {
     title: string
     description?: string
-    progress: number
-    totalLessons: number
-    completedLessons: number
-    completedTests: number
-    totalTests: number
-    approvedTests: number
     courseId: number
     children?: React.ReactNode
     img: string
 }) => (
-    <Card>
+    <Card className="h-full flex flex-col justify-between">
         <CardHeader>
             <CardTitle>{title}</CardTitle>
             {description && <CardDescription>{description}</CardDescription>}
         </CardHeader>
-        <CardContent
-            className='p-4'
-        >
-            <Link
-                className="p-4 flex justify-center w-full"
-                href={`/dashboard/student/courses/${courseId}`}
-            >
-                <img src={img}
-                    alt='Course Image' className="rounded-md object-cover"
-                />
+        <CardContent className="p-4 flex-grow">
+            <Link className="p-4 flex justify-center w-full" href={`/dashboard/student/courses/${courseId}`}>
+                <img src={img} alt="Course Image" className="rounded-md object-cover max-h-48 w-full" />
             </Link>
-            <div className="flex flex-wrap justify-center gap-4">
+            <div className="flex flex-wrap justify-center gap-4 mt-4">
                 <Link
-                    className={cn(
-                        buttonVariants({ variant: 'default' }),
-                        'flex items-center justify-center gap-2'
-                    )}
+                    className={cn(buttonVariants({ variant: 'default' }), 'flex items-center justify-center gap-2')}
                     href={`/dashboard/student/courses/${courseId}/lessons`}
                 >
                     <BookOpenTextIcon className="h-6 w-6" />
-            View Lessons
+          View Lessons
                 </Link>
                 <Link
-                    className={cn(
-                        buttonVariants({ variant: 'secondary' }),
-                        'flex items-center justify-center gap-2'
-                    )}
+                    className={cn(buttonVariants({ variant: 'secondary' }), 'flex items-center justify-center gap-2')}
                     href={`/dashboard/student/courses/${courseId}/exams`}
                 >
                     <PencilLineIcon className="h-6 w-6" />
-            View Exams
+          View Exams
                 </Link>
                 {children}
             </div>
-            {/* <div className="flex items-center justify-between">
-                <div>
-                    <p className="text-2xl font-bold">{progress}%</p>
-                    <p className="text-gray-500 dark:text-gray-400">
-            Total Progress
-                    </p>
-                </div>
-                <BarChart className="w-[100px] aspect-square" />
-            </div>
-            <div className="grid grid-cols-[1fr_1fr] gap-4 mt-6">
-                <div>
-                    <p className="text-2xl font-bold">{totalLessons}</p>
-                    <p className="text-gray-500 dark:text-gray-400">
-            Total Lessons
-                    </p>
-                </div>
-                <div>
-                    <p className="text-2xl font-bold">{completedLessons}</p>
-                    <p className="text-gray-500 dark:text-gray-400">
-            Lessons Completed
-                    </p>
-                </div>
-                <div>
-                    <p className="text-2xl font-bold">{completedTests}</p>
-                    <p className="text-gray-500 dark:text-gray-400">
-            Tests Completed
-                    </p>
-                </div>
-                <div>
-                    <p className="text-2xl font-bold">{totalTests}</p>
-                    <p className="text-gray-500 dark:text-gray-400">
-            Total Tests
-                    </p>
-                </div>
-                <div>
-                    <p className="text-2xl font-bold">{approvedTests}</p>
-                    <p className="text-gray-500 dark:text-gray-400">
-            Tests Approved
-                    </p>
-                </div>
-            </div> */}
         </CardContent>
     </Card>
 )
