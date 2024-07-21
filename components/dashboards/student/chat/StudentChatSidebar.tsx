379384b375a78971596922c8af9f3e9c529cbc7f
guillermoscript <arepayquezo@gmail.com>
2024-07-20 20:26:07 -0400

    feat: Update StudentChatSidebar to use SearchChats component
    The StudentChatSidebar component was modified to use the SearchChats component instead of the Command and Collapsible components. This change improves the code by simplifying the structure and enhancing the user experience with a more intuitive search functionality.

--- components/dashboards/student/chat/StudentChatSidebar.tsx
+++ components/dashboards/student/chat/StudentChatSidebar.tsx
@@ -1,23 +1,6 @@
-import { CaretSortIcon } from '@radix-ui/react-icons'
 
-import ChatSidebarItem from '@/components/dashboards/student/chat/ChatSidebarItem'
-import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger
-} from '@/components/ui/collapsible'
-import {
-    Command,
-    CommandEmpty,
-    CommandInput,
-    CommandItem,
-    CommandList
-} from '@/components/ui/command'
+import SearchChats from '@/components/dashboards/student/chat/SearchChats'
 import { createClient } from '@/utils/supabase/server'
-import { Tables } from '@/utils/supabase/supabase'
-
-import ChatCreationButton from './ChatCreationButton'
-import StudentCreateNewChat from './StudentCreateNewChat'
 
 export default async function StudentChatSidebar ({ userRole }) {
     const supabase = createClient()
@@ -41,68 +24,10 @@ export default async function StudentChatSidebar ({ userRole }) {
 
     return (
         <nav className="flex flex-col gap-2 h-auto justify-start w-full border-none items-start ">
-            <Command className="bg-gray-100/40 dark:bg-gray-800/40 border rounded-lg w-full">
-                <CommandInput placeholder="Type to search..." />
-                <CommandList className="w-full p-2 max-h-[calc(100vh-4rem)] overflow-y-auto ">
-                    <CommandEmpty>No results found.</CommandEmpty>
-                    <CommandItem asChild>
-                        <StudentCreateNewChat />
-                    </CommandItem>
-                    {Object.entries(chatTypes).map(([type, chats]) => {
-                        const types = {
-                            free_chat: 'free-chat',
-                            qna: 'qa',
-                            exam_prep: 'exam-prep',
-                            course_chat: 'study-material'
-                        }
-
-                        const title = {
-                            free_chat: 'Free Chat',
-                            qna: 'Q&A',
-                            exam_prep: 'Exam Prep',
-                            course_chat: 'Study Material'
-                        }
-
-                        return (
-                            <Collapsible
-                                id={type}
-                                defaultOpen={true} className="w-full"
-                            >
-                                <CollapsibleTrigger
-                                    className="w-full text-md font-semibold capitalize flex items-center justify-between p-2 px-2 rounded-lg my-2 hover:bg-opacity-10 dark:hover:bg-opacity-10 hover:bg-gray-400 dark:hover:bg-gray-200"
-                                >
-                                    <p>{type.replace('_', ' ')}</p>
-                                    <CaretSortIcon className="h-4 w-4" />
-                                </CollapsibleTrigger>
-                                <CollapsibleContent className="border-y border-gray-400 dark:border-gray-200">
-                                    <div
-                                        className='my-4 mx-1'
-                                    >
-                                        <ChatCreationButton
-                                            chatType={type as 'free_chat' | 'exam_prep'}
-                                            title={`Untitled ${title[type]}`}
-                                        />
-                                    </div>
-                                    {chats.map((chat: Tables<'chats'>) => {
-                                        return (
-                                            <>
-                                                <CommandItem value={chat.chat_id.toString()}>
-                                                    <ChatSidebarItem
-                                                        key={chat.chat_id}
-                                                        chat={chat}
-                                                        chatType={types[type]}
-                                                        userRole={userRole}
-                                                    />
-                                                </CommandItem>
-                                            </>
-                                        )
-                                    })}
-                                </CollapsibleContent>
-                            </Collapsible>
-                        )
-                    })}
-                </CommandList>
-            </Command>
+            <SearchChats
+                userRole={userRole}
+                chatTypes={chatTypes}
+            />
         </nav>
     )
 }
