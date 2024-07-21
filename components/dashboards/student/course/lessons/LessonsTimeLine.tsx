53bd3ff00910885c995d443ee82cb6443c5d677f
guillermoscript <arepayquezo@gmail.com>
2024-07-21 18:22:07 -0400

    refactor: Update LessonsTimeLine component to use ol element and separate TimelineItem into its own component
    This commit refactors the LessonsTimeLine component in the LessonsTimeLine.tsx file. It replaces the Timeline component with an ol element for better semantic structure. It also extracts the TimelineItem component into its own file for improved organization. These changes improve the readability and user experience of the LessonsTimeLine component in the application.

--- components/dashboards/student/course/lessons/LessonsTimeLine.tsx
+++ components/dashboards/student/course/lessons/LessonsTimeLine.tsx
@@ -1,19 +1,8 @@
-import dayjs from 'dayjs' // Assuming dayjs is already being used for date formatting
-import { CheckCircle, Clock } from 'lucide-react'
-import Link from 'next/link'
+// Assuming dayjs is already being used for date formatting
 
-import {
-    Timeline,
-    TimelineConnector,
-    TimelineContent,
-    TimelineDescription,
-    TimelineHeader,
-    TimelineIcon,
-    TimelineItem,
-    TimelineTime,
-    TimelineTitle
-} from '@/components/ui/timeline'
-import { cn } from '@/utils'
+import dayjs from 'dayjs'
+
+import TimelineItem from '@/components/ui/timeline'
 import { createClient } from '@/utils/supabase/server'
 
 export default async function LessonsTimeLine ({
@@ -39,58 +28,18 @@ export default async function LessonsTimeLine ({
     }
 
     return (
-        <Timeline
-            className='w-full py-4'
-        >
-            {items.map((item, index) => {
-                return (
-                    <Link
-                        href={`/dashboard/student/courses/${courseId}/lessons/${item.id}`}
-                        key={index}
-                        className={
-
-                            'hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md transition-colors duration-200 ease-in-out'
-
-                        }
-                    >
-                        <TimelineItem >
-                            <TimelineConnector />
-                            <TimelineHeader>
-                                <TimelineTime>{item.sequence}</TimelineTime>
-                                <TimelineIcon />
-                                <TimelineTitle
-                                    className={cn(
-                                        'text-lg',
-                                        item.id === lessonId && 'text-primary'
-                                    )}
-                                >
-                                    {item.title}</TimelineTitle>
-                            </TimelineHeader>
-                            <TimelineContent
-                                className='flex flex-col gap-3'
-                            >
-                                <TimelineDescription>{item.description}</TimelineDescription>
-                                <p>
-                                    {dayjs(item.created_at).format('MMMM D, YYYY')}
-                                </p>
-                                <div className='flex items-center gap-2'>
-                                    {item.lesson_completions?.length > 0 ? (
-                                        <>
-                                            <p className='text-sm font-semibold'>Completed</p>
-                                            <CheckCircle className='h-6 w-6 text-green-500' />
-                                        </>
-                                    ) : (
-                                        <>
-                                            <p className='text-sm font-semibold'>Not completed</p>
-                                            <Clock className='h-6 w-6' />
-                                        </>
-                                    )}
-                                </div>
-                            </TimelineContent>
-                        </TimelineItem>
-                    </Link>
-                )
-            })}
-        </Timeline>
+        <ol className="relative border-s border-gray-200 dark:border-gray-700 px-2">
+            {items.map((item, index) => (
+                <TimelineItem
+                    key={index}
+                    title={item.title}
+                    date={dayjs(item.created_at).format('MMM D, YYYY')}
+                    description={item.description}
+                    latest={item.id === lessonId}
+                    isCheck={item.lesson_completions.length > 0}
+                    link={`/dashboard/student/courses/${courseId}/lessons/${item.id}`}
+                />
+            ))}
+        </ol>
     )
 }
