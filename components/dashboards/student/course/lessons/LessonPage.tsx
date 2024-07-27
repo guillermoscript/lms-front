be4d822f7ce8e918e34fdf661505c6c90cd99e0d
guillermoscript <arepayquezo@gmail.com>
2024-07-27 16:04:41 -0400

    feat: Update LessonPage layout for responsive design
    The LessonPage component in `components/dashboards/student/course/lessons/LessonPage.tsx` has been updated to improve the layout for responsive design. The changes ensure that the content and sidebar are displayed properly on different screen sizes.

--- components/dashboards/student/course/lessons/LessonPage.tsx
+++ components/dashboards/student/course/lessons/LessonPage.tsx
@@ -1,31 +1,14 @@
-'use client'
-
-import { useMediaQuery } from 'usehooks-ts'
-
-import { Separator } from '@/components/ui/separator'
 export default function LessonPage({
     children,
     sideBar,
 }: {
-    children: React.ReactNode
-    sideBar: React.ReactNode
+    children: React.ReactNode;
+    sideBar: React.ReactNode;
 }) {
-    const matches = useMediaQuery('(min-width: 768px)')
-
-    if (matches) {
-        return (
-            <div className="flex flex-row gap-4">
-                <div className="w-[75%]">{children}</div>
-                <div className="flex-1 border-l">{sideBar}</div>
-            </div>
-        )
-    }
-
     return (
-        <div className="flex flex-col gap-4">
-            {children}
-            <Separator className="my-4" />
-            {sideBar}
+        <div className="flex flex-col gap-4 md:flex-row w-full">
+            <div className="flex flex-col w-full md:w-3/4">{children}</div>
+            <div className="flex-1 border-l w-full md:w-1/4">{sideBar}</div>
         </div>
     )
 }
