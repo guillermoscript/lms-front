ddc5e1545677f82931418ffccf3e91c7214a78a8
guillermoscript <arepayquezo@gmail.com>
2024-08-01 22:00:41 -0400

    refactor: Update Header component to use Next.js Image for logo
    Refactor the Header component to replace the SVG logo with a Next.js Image component. This change improves the performance and accessibility of the logo by optimizing its loading and rendering. It also aligns with best practices for using images in Next.js applications.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import { DollarSign, Menu, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import {
@@ -15,31 +16,18 @@ import { DarkThemeToggle } from './DarkThemeToggle'
 
 export default function Header({ children }: { children?: React.ReactNode }) {
     return (
-        <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 container">
+        <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ">
             <div className="flex items-center justify-between py-1 px-4">
-                <div className="flex gap-6 md:gap-10">
-                    <a
+                <div className="flex gap-6 md:gap-10 w-full">
+                    <Link
                         className="hidden items-center space-x-2 md:flex"
                         href="/"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width={24}
-                            height={24}
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-command"
-                        >
-                            <path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z" />
-                        </svg>
+
                         <span className="hidden font-bold sm:inline-block">
                             LMS
                         </span>
-                    </a>
+                    </Link>
                     <nav className="hidden gap-6 md:flex">
                         <Link
                             className="flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm text-foreground/60"
@@ -55,11 +43,17 @@ export default function Header({ children }: { children?: React.ReactNode }) {
                         </Link>
                     </nav>
                     <Sheet>
-                        <SheetTrigger className="flex items-center space-x-2 md:hidden p-3">
+                        <SheetTrigger className="flex items-center space-x-2 md:hidden p-3 justify-between gap-2 w-full">
                             <Menu className="h-6 w-6" />
+                            <Image
+                                src="/img/logo.png"
+                                alt="LMS"
+                                width={52}
+                                height={52}
+                            />
                         </SheetTrigger>
                         <SheetContent>
-                            <SheetHeader>
+                            <SheetHeader className='w-full'>
                                 <SheetTitle>LMS Academy</SheetTitle>
                                 <SheetDescription>
                                     <nav className="flex flex-col gap-4">
