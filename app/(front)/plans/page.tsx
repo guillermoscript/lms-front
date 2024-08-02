8921ec9876ef76c92671cffdc2a8281e39acf411
guillermoscript <arepayquezo@gmail.com>
2024-08-01 22:01:58 -0400

    refactor: Update PlansCheckoutPage, StorePage, UserLoginForm, UserSignupForm, AuthButton, Footer, Header, and authActions components
    Refactor multiple components to enhance user experience, flexibility, and reusability. Updates include adding login and signup forms to PlansCheckoutPage, using dynamic image source in StorePage, including redirect prop in UserLoginForm and UserSignupForm, using outlined variant for logout button in AuthButton, replacing icons in Footer with Lucide's Github icon, using Next.js Image for logo in Header, and handling redirect URL after sign in or sign up in authActions. These changes improve the visual appeal, maintainability, performance, and flexibility of the components.

--- app/(front)/plans/page.tsx
+++ app/(front)/plans/page.tsx
@@ -1,126 +1,19 @@
-import PriceCard from '@/components/plans/PriceCard'
+import SubscriptionPlan from '@/components/plans/PlansTabs'
 import { createClient } from '@/utils/supabase/server'
 
-export default async function PlanPage() {
+export default async function PlanPage () {
     const supabase = createClient()
 
-    const plans = await supabase.from('plans').select('*')
+    const plans = await supabase.from('plans').select('*').is('deleted_at', null)
 
     if (plans.error != null) {
         console.log(plans.error)
         throw plans.error
     }
 
-    const [month, quarterly, yearly] = plans.data
-
-    const basicData = [
-        {
-            title: 'Hobby',
-            price: month.price,
-            description: 'Best for developers trying to use the platform.',
-            features: [
-                { text: '5 API requests per day' },
-                { text: 'Access to basic API endpoints' },
-                { text: 'Email support within 48 hours' },
-                { text: 'Community forum access' },
-                { text: 'Monthly newsletter' },
-            ],
-            buttonText: 'Buy Now',
-            isHighlighted: false,
-            periodicity: 'month',
-            id: month.plan_id,
-        },
-        {
-            title: 'Starter',
-            price: quarterly.price,
-            description: 'Perfect for small businesses',
-            features: [
-                { text: 'Everything in Hobby, plus' },
-                { text: '50 API requests per day' },
-                { text: 'Access to advanced API endpoints' },
-                { text: 'Email support within 24 hours' },
-                { text: 'Community forum access' },
-                { text: 'Self hosting options' },
-            ],
-            buttonText: 'Buy Now',
-            isHighlighted: false,
-            periodicity: 'quarter',
-            id: quarterly.plan_id,
-        },
-        {
-            title: 'Professional',
-            price: yearly.price,
-            description: 'Ideal for small to mid range startups',
-            features: [
-                { text: 'Everything in Starter, plus' },
-                { text: '500 API requests per day' },
-                { text: 'Access to super advanced API endpoints' },
-                { text: 'Email support within 12 hours' },
-                { text: 'Private Community access' },
-                { text: 'Monthly retreats' },
-                { text: 'Self hosting options' },
-                { text: 'Private infrastructure' },
-                { text: 'On-Prem deployments' },
-            ],
-            buttonText: 'Buy Now',
-            isHighlighted: true,
-            periodicity: 'year',
-            id: yearly.plan_id,
-        },
-    ]
-
     return (
-        <section className="w-full py-12 md:py-24 lg:py-20 container ">
-            <div className="relative z-20 py-10 md:pt-15">
-                <h1 className="max-w-5xl mx-auto text-center tracking-tight font-medium text-black dark:text-white text-3xl md:text-5xl md:leading-tight">
-                    <span
-                        data-br=":ru9:"
-                        data-brr={1}
-                        style={{
-                            display: 'inline-block',
-                            verticalAlign: 'top',
-                            textDecoration: 'inherit',
-                            textWrap: 'balance',
-                        }}
-                    >
-                        Simple pricing for your ease
-                    </span>
-                </h1>
-                <h2 className="text-sm md:text-base max-w-4xl my-4 mx-auto  font-normal text-center">
-                    <span
-                        data-br=":rua:"
-                        data-brr={1}
-                        style={{
-                            display: 'inline-block',
-                            verticalAlign: 'top',
-                            textDecoration: 'inherit',
-                            textWrap: 'balance',
-                        }}
-                    >
-                        Every AI offers a wide range of services. You can choose
-                        the one that suits your needs. Select from your
-                        favourite plan and get started instantly.
-                    </span>
-                </h2>
-            </div>
-            <div className="mx-auto flex flex-col gap-6 sm:flex-row sm:justify-center sm:gap-6">
-                {basicData.map((data) => {
-                    return (
-                        <PriceCard
-                            title={data.title}
-                            price={data.price}
-                            description={data.description}
-                            features={data.features}
-                            buttonText={data.buttonText}
-                            isHighlighted={data.isHighlighted}
-                            periodicity={data.periodicity}
-                            key={data.id}
-                            id={data.id}
-                        />
-                    )
-                })}
-            </div>
-            {/* <div className="container px-4 md:px-6 flex flex-col gap-4 md:gap-8">
+        <section className="w-full py-12 md:py-24 lg:py-32 ">
+            <div className="container px-4 md:px-6 flex flex-col gap-4 md:gap-8">
                 <div className="space-y-6 text-center">
                     <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
                         Pricing Plans
@@ -130,34 +23,7 @@ export default async function PlanPage() {
                         plans come with a 30-day money-back guarantee.
                     </p>
                 </div>
-
-                <div className="mx-auto flex flex-col gap-6 sm:flex-row sm:justify-center sm:gap-6">
-                    <PlanCard
-                        title={month.plan_name}
-                        description={month.description}
-                        features={month.features}
-                        price={month.price}
-                        buttonVariant="secondary"
-                        planId={month.plan_id}
-                    />
-                    <PlanCard
-                        title={yearly.plan_name}
-                        description={yearly.description}
-                        features={yearly.features}
-                        price={yearly.price}
-                        oldPrice={400}
-                        isPopular
-                        buttonVariant="default"
-                        planId={quarterly.plan_id}
-                    />
-                    <PlanCard
-                        title={quarterly.plan_name}
-                        description={quarterly.description}
-                        features={quarterly.features}
-                        price={quarterly.price}
-                        buttonVariant="secondary"
-                    />
-                </div>
+                <SubscriptionPlan plans={plans.data} />
                 <div className="mt-12 space-y-4 text-center">
                     <p className="text-gray-500 dark:text-gray-400">
                         All plans come with a 30-day money-back guarantee, 24/7
@@ -173,7 +39,7 @@ export default async function PlanPage() {
                         service and features.
                     </p>
                 </div>
-            </div> */}
+            </div>
         </section>
     )
 }
