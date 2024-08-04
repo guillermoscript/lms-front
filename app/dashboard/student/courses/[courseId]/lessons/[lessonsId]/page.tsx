88abfe9eac15e263713418766eed3b33e89c934f
guillermoscript <arepayquezo@gmail.com>
2024-08-04 18:14:44 -0400

    ```text feat: Update SidebarLessons component in lesson page
    Refactor the SidebarLessons component in the lesson page to improve code readability and maintainability. This enhancement simplifies the component structure and removes unnecessary dependencies, resulting in cleaner and more efficient code.

--- app/dashboard/student/courses/[courseId]/lessons/[lessonsId]/page.tsx
+++ app/dashboard/student/courses/[courseId]/lessons/[lessonsId]/page.tsx
@@ -1,13 +1,7 @@
-import { Suspense } from 'react'
-
-import CommentsSections from '@/components/dashboards/Common/CommentsSections'
-import ListOfReviews from '@/components/dashboards/Common/reviews/ListOfReviews'
 import LessonContent from '@/components/dashboards/student/course/lessons/LessonContent'
 import LessonPage from '@/components/dashboards/student/course/lessons/LessonPage'
-import LessonsTimeLine from '@/components/dashboards/student/course/lessons/LessonsTimeLine'
-import TableOfContents from '@/components/dashboards/student/course/lessons/LessonTableOfContent'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { createClient } from '@/utils/supabase/server'
+import SidebarLessons from '@/components/dashboards/Common/lessons/SidebarLessons'
 
 export const metadata = {
     title: 'Student Lesson Page',
@@ -48,7 +42,7 @@ export default async function StudentLessonPage({
     return (
         <LessonPage
             sideBar={
-                <Sidebar
+                <SidebarLessons
                     courseId={Number(params.courseId)}
                     lessonId={lessonData.data.id}
                     lessonData={lessonData.data}
@@ -67,49 +61,3 @@ export default async function StudentLessonPage({
     )
 }
 
-function Sidebar({
-    courseId,
-    lessonId,
-    lessonData,
-}: {
-    courseId: number
-    lessonId: number
-    lessonData: any
-}) {
-    return (
-        <Tabs
-            defaultValue="comments"
-            className="w-full flex items-center justify-center flex-col h-auto"
-        >
-            <TabsList className="flex-col md:flex-row md:flex-wrap gap-2 h-auto md:gap-4 w-full md:w-auto mx-0 md:mx-3">
-                <TabsTrigger value="comments">Comments</TabsTrigger>
-                <TabsTrigger value="timeline">Timeline</TabsTrigger>
-                <TabsTrigger value="tableOfContents">
-                    Table of Contents
-                </TabsTrigger>
-                <TabsTrigger value="reviews">Reviews</TabsTrigger>
-            </TabsList>
-            <TabsContent value="comments">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <CommentsSections
-                        course_id={courseId}
-                        lesson_id={lessonId}
-                        lesson_comments={lessonData.lesson_comments}
-                    />
-                </Suspense>
-            </TabsContent>
-            <TabsContent className="p-4 md:p-6" value="timeline">
-                <LessonsTimeLine courseId={courseId} lessonId={lessonId} />
-            </TabsContent>
-            <TabsContent className="p-4 md:p-6" value="tableOfContents">
-                <TableOfContents markdown={lessonData.content} />
-            </TabsContent>
-            <TabsContent
-                className="p-4 md:p-6 flex flex-col gap-4"
-                value="reviews"
-            >
-                <ListOfReviews entityId={lessonId} entityType="lessons" />
-            </TabsContent>
-        </Tabs>
-    )
-}
