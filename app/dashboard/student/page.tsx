3f66168a84499209063213d1fb75d4cb0e8e1f4a
guillermoscript <arepayquezo@gmail.com>
2024-08-03 21:07:39 -0400

    feat: Refactor ChatsSectionComponent to improve code readability and maintainability
    Refactor the ChatsSectionComponent in page.tsx to improve code readability and maintainability. This change includes extracting the chat item rendering logic into a separate component, making the code more modular and easier to understand. By refactoring the component, we enhance the overall quality of the codebase and make it easier to maintain and extend in the future.

--- app/dashboard/student/page.tsx
+++ app/dashboard/student/page.tsx
@@ -8,6 +8,7 @@ import RecentlyViewed from '@/components/dashboards/student/course/lessons/Recen
 import { Badge } from '@/components/ui/badge'
 import { createClient } from '@/utils/supabase/server'
 import { Tables } from '@/utils/supabase/supabase'
+
 export default async function CoursesStudentPage() {
     const supabase = createClient()
     const user = await supabase.auth.getUser()
@@ -77,43 +78,45 @@ function ChatsSectionComponent({ chats }: { chats: Array<Tables<'chats'>> }) {
                 </h3>
             </div>
             <div className="flex flex-wrap gap-4 w-full justify-around items-center">
-                {chats.map((chat) => (
-                    <div
-                        className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 border border-neutral-200 dark:border-neutral-700 rounded-lg p-4 flex flex-col gap-2 items-start"
-                        key={chat.chat_id}
-                    >
-                        <div className="flex items-center justify-between w-full">
-                            <div className='flex items-center gap-2'>
-                                {chat.chat_type === 'free_chat' ? (
-                                    <Badge
-                                        variant='outline'
-                                        className="text-sm text-neutral-600 dark:text-neutral-300"
-                                    >
-                                Free Chat
-                                    </Badge>
-                                ) : chat.chat_type === 'exam_prep' ? (
-                                    <Badge
-                                        variant='outline'
-                                        className="text-sm text-neutral-600 dark:text-neutral-300"
-                                    >
-                                Exam Prep
-                                    </Badge>
-                                ) : null}
-                            </div>
-                            <p>
-                                {dayjs(chat.created_at).format('MMMM D, YYYY')}
-                            </p>
-                        </div>
-                        <Link href={`/dashboard/student/chats/${chat.chat_id}`}
-                            className="p-4 border-b border-neutral-200 dark:border-neutral-700 w-full"
+                {chats.map((chat) => {
+                    return (
+                        <div
+                            className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 border border-neutral-200 dark:border-neutral-700 rounded-lg p-4 flex flex-col gap-2 items-start"
+                            key={chat.chat_id}
                         >
-                            <h4 className="text-lg font-semibold text-neutral-900 dark:text-neutral-200">
-                                {chat.title.slice(0, 30) +
-                                    (chat.title.length > 30 ? '...' : '')}
-                            </h4>
-                        </Link>
-                    </div>
-                ))}
+                            <div className="flex items-center justify-between w-full">
+                                <div className='flex items-center gap-2'>
+                                    {chat.chat_type === 'free_chat' ? (
+                                        <Badge
+                                            variant='outline'
+                                            className="text-sm text-neutral-600 dark:text-neutral-300"
+                                        >
+                                    Free Chat
+                                        </Badge>
+                                    ) : chat.chat_type === 'exam_prep' ? (
+                                        <Badge
+                                            variant='outline'
+                                            className="text-sm text-neutral-600 dark:text-neutral-300"
+                                        >
+                                    Exam Prep
+                                        </Badge>
+                                    ) : null}
+                                </div>
+                                <p>
+                                    {dayjs(chat.created_at).format('MMMM D, YYYY')}
+                                </p>
+                            </div>
+                            <Link href={`/dashboard/student/chat/${chat.chat_id}/${chat.chat_type.replace('_', '-')}`}
+                                className="p-4 border-b border-neutral-200 dark:border-neutral-700 w-full"
+                            >
+                                <h4 className="text-lg font-semibold text-neutral-900 dark:text-neutral-200">
+                                    {chat.title.slice(0, 30) +
+                                        (chat.title.length > 30 ? '...' : '')}
+                                </h4>
+                            </Link>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
