442a88e4e3a0c4888ff9871df0731051cfed088c
guillermoscript <arepayquezo@gmail.com>
2024-08-07 22:49:11 -0400

    fix: Update GenericError component link to /contact/support

--- components/GenericError.tsx
+++ components/GenericError.tsx
@@ -31,7 +31,7 @@ export default function GenericError ({ retry, children, title, description }: P
                         </Button>
                         <Link
                             className="inline-flex items-center justify-center px-4 py-2 bg-gray-900 hover:bg-gray-800 text-white rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-                            href="#"
+                            href="/contact/support"
                         >
               Contact Support
                         </Link>
