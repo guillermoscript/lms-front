a312f3c83c88560a45f289639b6bb76ca4eea641
guillermoscript <arepayquezo@gmail.com>
2024-08-08 22:39:58 -0400

    feat: Add ChatTabs component for managing chat input options

--- components/dashboards/student/course/lessons/TaksMessages.tsx
+++ components/dashboards/student/course/lessons/TaksMessages.tsx
@@ -8,13 +8,63 @@ import { UIState } from '@/actions/dashboard/AI/FreeChatPreparation'
 import { TaskAiActions } from '@/actions/dashboard/AI/TaskAiActions'
 import { studentSubmitAiTaskMessage } from '@/actions/dashboard/lessonsAction'
 import ChatLoadingSkeleton from '@/components/dashboards/chat/ChatLoadingSkeleton'
-import {
-    ChatInput,
-    SuccessMessage,
-} from '@/components/dashboards/Common/chat/chat'
+import { ChatInput, ChatTextArea, SuccessMessage } from '@/components/dashboards/Common/chat/chat'
 import Message from '@/components/dashboards/Common/chat/Message'
 import ViewMarkdown from '@/components/ui/markdown/ViewMarkdown'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import useNoCopy from '@/utils/hooks/useNoCopy'
+
+async function handleSubmitMessage({
+    input,
+    setConversation,
+    setIsLoading,
+    stop,
+    continueTaskAiConversation,
+    lessonId,
+}: {
+    input: string
+    setConversation: (value: any) => void
+    setIsLoading: (value: boolean) => void
+    stop: boolean
+    continueTaskAiConversation: (content: string) => Promise<ClientMessage>
+    lessonId: number
+}) {
+    if (stop) return
+
+    setIsLoading(true)
+
+    setConversation((currentConversation: ClientMessage[]) => [
+        ...currentConversation,
+        {
+            id: generateId(),
+            role: 'user',
+            display: (
+                <Message
+                    sender={'user'}
+                    time={new Date().toDateString()}
+                    isUser={true}
+                >
+                    <ViewMarkdown markdown={input} />
+                </Message>
+            ),
+        },
+    ])
+
+    const res = await studentSubmitAiTaskMessage({
+        lessonId,
+        message: {
+            content: input,
+            role: 'user',
+        },
+    })
+
+    const message = await continueTaskAiConversation(input)
+    setConversation((currentConversation: ClientMessage[]) => [
+        ...currentConversation,
+        message,
+    ])
+    setIsLoading(false)
+}
 
 export default function TaksMessages({
     lessonId,
@@ -49,56 +99,46 @@ export default function TaksMessages({
                 <div className="w-full h-px" />
                 {!isLastMessageFromMakeUserAssigmentCompleted &&
                     !isLessonAiTaskCompleted && (
-                    <ChatInput
-                        isLoading={isLoading}
-                        stop={() => setStop(true)}
-                        callbackFunction={async (input) => {
-                            if (stop) return
-
-                            setIsLoading(true)
-
-                            setConversation(
-                                (currentConversation: ClientMessage[]) => [
-                                    ...currentConversation,
-                                    {
-                                        id: generateId(),
-                                        role: 'user',
-                                        display: (
-                                            <Message
-                                                sender={'user'}
-                                                time={new Date().toDateString()}
-                                                isUser={true}
-                                            >
-                                                <ViewMarkdown
-                                                    markdown={input.content}
-                                                />
-                                            </Message>
-                                        ),
-                                    },
-                                ]
-                            )
-
-                            const res = await studentSubmitAiTaskMessage({
-                                lessonId,
-                                message: {
-                                    content: input.content,
-                                    role: 'user',
-                                },
-                            })
-
-                            const message =
-                                    await continueTaskAiConversation(
-                                        input.content
-                                    )
-                            setConversation(
-                                (currentConversation: ClientMessage[]) => [
-                                    ...currentConversation,
-                                    message,
-                                ]
-                            )
-                            setIsLoading(false)
-                        }}
-                    />
+                    <>
+                        <Tabs defaultValue="simple" className="w-full py-4">
+                            <TabsList>
+                                <TabsTrigger value="simple">Simple</TabsTrigger>
+                                <TabsTrigger value="markdown">Markdown</TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="markdown">
+                                <ChatInput
+                                    isLoading={isLoading}
+                                    stop={() => setStop(true)}
+                                    callbackFunction={async (input) => {
+                                        handleSubmitMessage({
+                                            input: input.content,
+                                            setIsLoading,
+                                            setConversation,
+                                            continueTaskAiConversation,
+                                            stop,
+                                            lessonId,
+                                        })
+                                    }}
+                                />
+                            </TabsContent>
+                            <TabsContent value="simple">
+                                <ChatTextArea
+                                    isLoading={isLoading}
+                                    stop={() => setStop(true)}
+                                    callbackFunction={async (input) => {
+                                        handleSubmitMessage({
+                                            input: input.content,
+                                            setIsLoading,
+                                            setConversation,
+                                            continueTaskAiConversation,
+                                            stop,
+                                            lessonId,
+                                        })
+                                    }}
+                                />
+                            </TabsContent>
+                        </Tabs>
+                    </>
                 )}
 
                 {isLessonAiTaskCompleted && (
@@ -123,10 +163,7 @@ function ChatList({ messages, messagesEndRef }: ChatListProps) {
     useNoCopy(contentRef)
 
     return (
-        <div
-            ref={contentRef}
-            className="relative"
-        >
+        <div ref={contentRef} className="relative">
             {messages.map((message, index) => (
                 <div key={index} className="flex flex-col gap-2">
                     {message.display}
