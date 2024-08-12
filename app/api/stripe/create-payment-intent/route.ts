8596ec4a9361381f85e6e86ac1a9aed5812d4433
guillermoscript <arepayquezo@gmail.com>
2024-08-11 22:26:05 -0400

    refactor: Update Stripe payment amount calculation
    This commit updates the calculation of the payment amount for Stripe. Instead of directly multiplying the price by 100, it now uses the BigNumber library to ensure accurate calculations. This improves the reliability and accuracy of the payment process.

--- app/api/stripe/create-payment-intent/route.ts
+++ app/api/stripe/create-payment-intent/route.ts
@@ -1,5 +1,6 @@
 import { type NextRequest } from 'next/server'
 import Stripe from 'stripe'
+import BigNumber from "bignumber.js";
 
 import { createClient } from '@/utils/supabase/server'
 
@@ -83,9 +84,13 @@ export async function POST (req: NextRequest) {
 
         console.log(transaction, '<----------- transaction')
 
+        const price = new BigNumber(plan.data.price).multipliedBy(100).toNumber()
+
+        console.log(price, '<----------- price')
+
         // Create a PaymentIntent with the order amount and currency
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: plan.data.price * 100,
+            amount: price,
             currency: 'usd',
             customer: stripeCustomerID,
             setup_future_usage: 'off_session',
@@ -131,9 +136,11 @@ export async function POST (req: NextRequest) {
             .select('*')
             .single()
 
+        const price = new BigNumber(product.data.price).multipliedBy(100).toNumber()
+
         // Create a PaymentIntent with the order amount and currency
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: product.data.price * 100,
+            amount: price,
             customer: stripeCustomerID,
             setup_future_usage: 'off_session',
             currency: 'usd',
