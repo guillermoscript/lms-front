1f776b1ea6b52c7ce4f93ef1be9983575d40ae95
guillermoscript <arepayquezo@gmail.com>
2024-08-16 22:28:44 -0400

    refactor: Update UserLoginForm component with zod form validation and use-toast hook

--- components/auth/UserLoginForm.tsx
+++ components/auth/UserLoginForm.tsx
@@ -1,25 +1,68 @@
 'use client'
-import { Link } from 'lucide-react'
-import { useFormState, useFormStatus } from 'react-dom'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { EyeIcon, EyeOffIcon, Link } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { signIn } from '@/actions/auth/authActions'
 import { Button } from '@/components/ui/button'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { toast } from '@/components/ui/use-toast'
 
-import PasswordComponent from './PasswordComponent'
+// Define your Zod schema for validation
+const FormSchema = z.object({
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z
+        .string()
+        .min(8, { message: 'Password must be at least 8 characters.' })
+        .max(100, { message: 'Password must be at most 100 characters.' })
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, {
+            message:
+                'Password must contain at least one uppercase letter, one lowercase letter, and one number.',
+        }),
+})
 
-export default function UserLoginForm({
-    redirect,
-}: {
-    redirect: string
-}) {
-    const [state, action] = useFormState(signIn, {
-        status: 'idle',
-        message: '',
-        error: null,
+export default function UserLoginForm({ redirect }: { redirect?: string }) {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        },
     })
 
+    const [showPassword, setShowPassword] = useState(false)
+
+    const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+        try {
+            // Assuming signIn function returns a promise
+            const response = await signIn({
+                email: data.email,
+                password: data.password,
+                redirectTo: redirect,
+            })
+        } catch (error: any) {
+            // Handle login errors (e.g., display error messages)
+            toast({
+                title: 'Login Error',
+                description: error.message || 'An error occurred during login.',
+                variant: 'destructive',
+            })
+        }
+    }
+
     return (
-        <div className="flex-1 flex flex-col w-full sm:max-w-md justify-center gap-2">
+        <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
             <Link
                 href="/"
                 className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
@@ -38,49 +81,61 @@ export default function UserLoginForm({
                 >
                     <polyline points="15 18 9 12 15 6" />
                 </svg>{' '}
-                Back
+        Back
             </Link>
-
-            <form
-                className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-                action={action}
-            >
-                <label className="text-md" htmlFor="email">
-                    Email
-                </label>
-                <input
-                    className="rounded-md px-4 py-2 bg-inherit border mb-6"
-                    name="email"
-                    placeholder="you@example.com"
-                    required
-                />
-                <label className="text-md" htmlFor="password">
-                    Password
-                </label>
-                <input type="hidden" name="redirect" value={redirect} />
-                <PasswordComponent />
-                <SubmitButton />
-                {state.error && (
-                    <p className="mt-4 p-4 bg-destructive/80 text-foreground text-center">
-                        {state.message}
-                    </p>
-                )}
-            </form>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="animate-in flex-1 flex flex-col w-full justify-center gap-4 text-foreground"
+                >
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="you@example.com" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <div className=" w-full gap-4 flex items-center justify-between">
+                                        <Input
+                                            type={showPassword ? 'text' : 'password'}
+                                            name="password"
+                                            placeholder="••••••••"
+                                            {...field}
+                                        />
+                                        <button type="button" onClick={() => setShowPassword(!showPassword)}>
+                                            {!showPassword ? <EyeIcon /> : <EyeOffIcon />}
+                                        </button>
+                                    </div>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    {/* Optional hidden input for redirect if needed */}
+                    {redirect && (
+                        <input type="hidden" name="redirect" value={redirect} />
+                    )}
+                    <Button
+                        disabled={form.formState.isSubmitting}
+                        type="submit"
+                    >
+                        {form.formState.isSubmitting ? 'Logging in...' : 'Login'}
+                    </Button>
+                </form>
+            </Form>
         </div>
-    )
-}
-
-function SubmitButton() {
-    const { pending } = useFormStatus()
-    return (
-        <>
-            <Button
-                type="submit"
-                className="disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed w-full"
-                disabled={pending}
-            >
-                {pending ? 'Submitting...' : 'Login'}
-            </Button>
-        </>
     )
 }
