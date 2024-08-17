2881da0392d375990c3bf284d3d0bb36b4cf9752
guillermoscript <arepayquezo@gmail.com>
2024-08-17 19:04:53 -0400

    feat: Add MessageFeaturesSection component
    This commit adds a new file, MessageFeaturesSection.tsx, which contains the implementation of the MessageFeaturesSection component. This component provides buttons for managing the view mode and role in the chat interface. It includes icons for viewing, editing, and regenerating AI responses based on the current view mode and user role.

---
+++ components/dashboards/Common/chat/MessageFeaturesSection.tsx
@@ -0,0 +1,105 @@
+'use client'
+
+import { Eye, Loader, Pencil, Sparkle } from 'lucide-react'
+import { ViewMode } from './MessageContentWrapper'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+export default function MessageFeatureSection({
+    viewMode,
+    setViewMode,
+    role,
+}: {
+    viewMode: ViewMode
+    setViewMode: (value: ViewMode) => void
+    role: 'assistant' | 'user'
+}) {
+    return (
+        <div className="flex justify-start overflow-x-auto buttons text-gray-600 dark:text-gray-500 svelte-1u5gq5j">
+            <div className="flex">
+                {role === 'user' && (
+                    <button
+                        onClick={() => {
+                            console.log('Edit')
+                            if (viewMode === 'view') {
+                                setViewMode('edit')
+                            } else {
+                                setViewMode('view')
+                            }
+                        }}
+                        className="visible p-1.5 hover:bg-black/5 dark:hover:bg-white/5 rounded-lg dark:hover:text-white hover:text-black transition"
+                    >
+                        {viewMode === 'edit' ? (
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <Eye className="w-4 h-4" />
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>View Mode</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        ) : (
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <Pencil className="w-4 h-4" />
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Edit Mode</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        )}
+                    </button>
+                )}
+
+                {role === 'assistant' && (
+                    <button
+                        onClick={() => {
+                            console.log('Edit')
+                            if (viewMode === 'view') {
+                                setViewMode('regenerate')
+                            } else {
+                                setViewMode('view')
+                            }
+                        }}
+                        className="visible p-1.5 hover:bg-black/5 dark:hover:bg-white/5 rounded-lg dark:hover:text-white hover:text-black transition"
+                    >
+                        {viewMode === 'view' ? (
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <Sparkle className="w-4 h-4" />
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Regenerate the AI response</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        ) : (
+                            <div className="flex items-center justify-center animate-spin">
+                                <Loader className="w-4 h-4" />
+                            </div>
+                        )}
+                    </button>
+                )}
+            </div>
+            {/* <div aria-label="Delete" className="flex">
+                <button
+                    onClick={() => {
+                        console.log('Delete')
+                    }}
+                    className="visible p-1.5 hover:bg-black/5 dark:hover:bg-white/5 rounded-lg dark:hover:text-white hover:text-black transition"
+                >
+                    <Trash className="w-4 h-4" />
+                </button>
+            </div> */}
+        </div>
+    )
+}
