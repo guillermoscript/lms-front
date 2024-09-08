26a60daa38a22e98c47bd0ad71adbe4067a9743b
guillermoscript <arepayquezo@gmail.com>
2024-09-07 20:49:46 -0400

    refactor: Update resetPasswordFun to handle session code exchange

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -19,20 +19,3 @@ export async function GET (request: Request) {
     // URL to redirect to after sign in process completes
     return NextResponse.redirect(requestUrl.origin)
 }
-
-// import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-// import { cookies } from 'next/headers'
-// import { NextRequest, NextResponse } from 'next/server'
-
-// export async function GET(req: NextRequest) {
-//   const cookieStore = cookies()
-//   const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
-//   const { searchParams } = new URL(req.url)
-//   const code = searchParams.get('code')
-
-//   if (code) {
-//     await supabase.auth.exchangeCodeForSession(code)
-//   }
-
-//   return NextResponse.redirect(new URL('/account', req.url))
-// }
