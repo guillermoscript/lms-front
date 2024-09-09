c62831d514e27d9bcdce860f5fd0c2f9549d26ff
guillermoscript <arepayquezo@gmail.com>
2024-09-09 18:37:12 -0400

    Refactor page component to add HeroVideoDialogDemoTopInBottomOut and RetroGridDemo components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import HeroVideoDialogDemoTopInBottomOut from '@/components/example/hero-video-dialog-demo-top-in-bottom-out'
+import { RetroGridDemo } from '@/components/example/RetroGridDemo'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import { FeaturesSection } from '@/components/home/FeaturesSection'
-import HeroSection from '@/components/home/HeroSection'
 import OptionsSection from '@/components/home/OptionsSection'
 import ParticlesSection from '@/components/home/ParticlesSection'
 import WaitingList from '@/components/home/WaitingList'
@@ -12,11 +13,15 @@ export default async function Index() {
             <Header />
             <div className="max-w-7xl mx-auto px-4 flex flex-col items-center justify-between">
                 <ParticlesSection>
-                    <HeroSection />
+                    <HeroVideoDialogDemoTopInBottomOut />
+                    {/* <HeroSection /> */}
                 </ParticlesSection>
                 <FeaturesSection />
                 <OptionsSection />
                 <WaitingList />
+                <div className="w-full pb-12 lg:pb-24">
+                    <RetroGridDemo />
+                </div>
             </div>
             <Footer />
         </>
