6698a82e8a0b79d3adf78fa9acfa53e390e1dcea
guillermoscript <arepayquezo@gmail.com>
2024-09-09 18:42:52 -0400

    Refactor RetroGridDemo component to improve image styling

--- components/example/RetroGridDemo.tsx
+++ components/example/RetroGridDemo.tsx
@@ -4,13 +4,13 @@ import RetroGrid from '@/components/magicui/retro-grid'
 
 export function RetroGridDemo() {
     return (
-        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden ">
             <Image
                 src="/img/tengo-fe.jpg"
                 alt="Mano Tengo Fe"
                 width={300}
                 height={300}
-                className="rounded"
+                className="rounded z-10"
             />
             <RetroGrid />
         </div>
