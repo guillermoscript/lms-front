26213d5cd0b58c7e0b007b0edfdefe2b68d5ef03
guillermoscript <arepayquezo@gmail.com>
2024-09-23 15:08:35 -0400

    Refactor DeleteCourseAlert to use localized strings for the item type

--- components/dashboards/teacher/course/DeleteCourseAlert.tsx
+++ components/dashboards/teacher/course/DeleteCourseAlert.tsx
@@ -1,13 +1,16 @@
-
+'use client'
 import { deleteCourseAction } from '@/actions/dashboard/courseActions'
+import { useScopedI18n } from '@/app/locales/client'
 
 import DeleteAlert from '../DeleteAlert'
 
 export default function DeleteCourseAlert ({ courseId }: { courseId: number }) {
+    const t = useScopedI18n('DeleteCourseAlert')
+
     return (
         <DeleteAlert
             itemId={courseId.toString()}
-            itemType="Course"
+            itemType={t('course')}
             deleteAction={async (id: string) => await deleteCourseAction({ courseId: id })}
         />
     )
