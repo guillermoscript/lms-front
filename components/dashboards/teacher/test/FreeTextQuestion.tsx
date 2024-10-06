2d436bcfcff7c1e650247b5862149f0b816f2555
guillermoscript <arepayquezo@gmail.com>
2024-10-06 18:46:30 -0400

    change to text area

--- components/dashboards/teacher/test/FreeTextQuestion.tsx
+++ components/dashboards/teacher/test/FreeTextQuestion.tsx
@@ -11,6 +11,7 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
 
 interface Props {
     questions: Array<{ id: string, label: string, placeholder: string }>
@@ -34,7 +35,7 @@ const FreeTextQuestionForm: React.FC<Props> = ({ questions, control }) => (
                         </FormLabel>
 
                         <FormControl>
-                            <Input
+                            <Textarea
                                 {...field}
                                 id={question.id}
                                 placeholder={question.placeholder}
