7f6733cb9c5c7edfc0518d05876f707904734927
guillermoscript <arepayquezo@gmail.com>
2024-10-11 15:54:23 -0400

    Refactor CoursesLayout component to remove unused code and improve performance

--- app/[locale]/dashboard/student/courses/layout.tsx
+++ app/[locale]/dashboard/student/courses/layout.tsx
@@ -1,5 +1,3 @@
-import NoCoruseOrSubAlert from '@/components/dashboards/student/NoCoruseOrSubAlert'
-import { createClient } from '@/utils/supabase/server'
 
 export const metadata = {
     title: 'Courses Layout',
@@ -11,35 +9,5 @@ export default async function CoursesLayout ({
 }: {
     children: React.ReactNode
 }) {
-    const supabase = createClient()
-    const user = await supabase.auth.getUser()
-
-    if (user.error != null) {
-        throw new Error(user.error.message)
-    }
-
-    const userCourses = await supabase
-        .from('enrollments')
-        .select('enrollment_id')
-        .eq('user_id', user.data.user.id)
-
-    const userSubscriptions = await supabase
-        .from('subscriptions')
-        .select('subscription_id')
-        .eq('user_id', user.data.user.id)
-        .eq('subscription_status', 'active')
-
-    if (userSubscriptions.error != null && userCourses.error != null) {
-        throw new Error(
-            'Something went wrong while fetching your courses and subscriptions.'
-        )
-    }
-
-    if (userSubscriptions.data.length === 0 && userCourses.data.length === 0) {
-        return (
-            <NoCoruseOrSubAlert />
-        )
-    }
-
     return <>{children}</>
 }
