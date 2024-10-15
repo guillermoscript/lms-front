cf1f4988ddc0acb5185c0b9d2c987328bff990b4
guillermoscript <arepayquezo@gmail.com>
2024-10-15 18:04:39 -0400

    Refactor StudentExercisePage component to add spacing between elements

--- components/dashboards/exercises/StudentExercisePage.tsx
+++ components/dashboards/exercises/StudentExercisePage.tsx
@@ -113,6 +113,7 @@ export default function StudentExercisePage({
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
+                className='space-y-4'
             >
                 <Link
                     className={buttonVariants({ variant: 'ghost' })}
