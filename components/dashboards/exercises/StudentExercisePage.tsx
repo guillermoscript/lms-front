909481f26ce9717165b580dbd6e93b701dcf3c44
guillermoscript <arepayquezo@gmail.com>
2024-10-18 13:49:18 -0400

    Refactor StudentExercisePage layout to display exercise cards in a responsive grid

--- components/dashboards/exercises/StudentExercisePage.tsx
+++ components/dashboards/exercises/StudentExercisePage.tsx
@@ -163,7 +163,7 @@ export default function EnhancedStudentExercisePage({
                     </CardContent>
                 </Card>
 
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8">
+                <div className="md:grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 flex flex-col-reverse lg:flex-row">
                     <div className="lg:col-span-2 space-y-4">
                         <Card>
                             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
