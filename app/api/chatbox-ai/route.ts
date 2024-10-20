53923d7a42192be943dd2381d270dcf7898aef63
guillermoscript <arepayquezo@gmail.com>
2024-10-20 12:49:40 -0400

    Refactor chatbox-ai route and add TavilyClient and AISDKTools
    This commit refactors the chatbox-ai route by adding the TavilyClient and AISDKTools from the @agentic/ai-sdk and @agentic/tavily dependencies. It also updates the model to use the latest version of the Google gemini-1.5-pro. Additionally, it introduces a new function called foolowUpQuestions that provides a helpful set of follow-up questions to ask the teacher about the topic.

--- app/api/chatbox-ai/route.ts
+++ app/api/chatbox-ai/route.ts
@@ -1,7 +1,9 @@
+import { createAISDKTools } from '@agentic/ai-sdk'
+import { TavilyClient } from '@agentic/tavily'
 import { google } from '@ai-sdk/google'
 import { convertToCoreMessages, Message, streamText } from 'ai'
 import { NextRequest, NextResponse } from 'next/server'
-
+import { z } from 'zod'
 interface RequestBody {
     messages: Message[];
 }
@@ -9,12 +11,25 @@ interface RequestBody {
 export async function POST(request: NextRequest) {
     try {
         const reqBody: RequestBody = await request.json()
-        const model = google('gemini-1.5-flash')
+        const tavilSearch = new TavilyClient()
+        const model = google('gemini-1.5-pro-latest')
 
         const result = await streamText({
             model,
             messages: convertToCoreMessages(reqBody.messages),
             temperature: 0.7,
+            tools: {
+                ...createAISDKTools(tavilSearch),
+                foolowUpQuestions: {
+                    description: 'Function to provide a helpful set of follow-up questions to ask the teacher about the topic. Respond using the language of the student.',
+                    parameters: z.object({
+                        questions: z.array(z.string()).describe('List of follow-up questions to ask the teacher about the topic.'),
+                    }),
+                    execute: async ({ hints }) => {
+                        return hints
+                    }
+                }
+            }
         })
 
         return result.toDataStreamResponse()
