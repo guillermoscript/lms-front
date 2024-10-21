afe9b185705551cdd285f4a1d4a978bfc3aa2479
guillermoscript <arepayquezo@gmail.com>
2024-10-21 14:53:38 -0400

    Refactor AccountEditPage component to use scoped i18n and update breadcrumb links

--- app/[locale]/dashboard/student/account/edit/page.tsx
+++ app/[locale]/dashboard/student/account/edit/page.tsx
@@ -1,65 +1,27 @@
-import EditProfileForm from '@/components/dashboards/student/account/EditProfileForm'
+import { getScopedI18n } from '@/app/locales/server'
+import AccountEditPage from '@/components/dashboards/Common/account/AccountEditPage'
 import BreadcrumbComponent from '@/components/dashboards/student/course/BreadcrumbComponent'
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle
-} from '@/components/ui/card'
-import { createClient } from '@/utils/supabase/server'
 
 export default async function Dashboard () {
-    const supabase = createClient()
-    const {
-        data: { user },
-        error
-    } = await supabase.auth.getUser()
-
-    if (error != null) {
-        throw new Error(error.message)
-    }
-
-    const userProfile = await supabase
-        .from('profiles')
-        .select('full_name, bio, avatar_url')
-        .eq('id', user?.id)
-        .single()
+    const t = await getScopedI18n('BreadcrumbComponent')
 
     return (
-        <>
+        <div className="container mx-auto">
             <BreadcrumbComponent
                 links={[
-                    { href: '/dashboard', label: 'Dashboard' },
-                    { href: '/dashboard/student', label: 'Student' },
-                    { href: '/dashboard/student/account', label: 'Account' },
-                    { href: '/dashboard/student/account/edit', label: 'Edit' }
+                    { href: '/dashboard', label: t('dashboard') },
+                    { href: '/dashboard/student', label: t('student') },
+                    {
+                        href: '/dashboard/student/account/',
+                        label: t('account'),
+                    },
+                    {
+                        href: '/dashboard/student/account/edit',
+                        label: t('edit'),
+                    },
                 ]}
             />
-            <div className="mx-auto grid w-full max-w-6xl gap-2">
-                <h1 className="text-3xl font-semibold">Settings</h1>
-            </div>
-            <div className="mx-auto grid w-full max-w-6xl gap-2">
-                <div className="grid gap-6">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>
-                Edit Profile
-                            </CardTitle>
-                            <CardDescription>
-                Update your profile information
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <EditProfileForm
-                                fullName={userProfile.data.full_name}
-                                bio={userProfile.data.bio}
-                                avatarUrl={userProfile.data.avatar_url}
-                            />
-                        </CardContent>
-                    </Card>
-                </div>
-            </div>
-        </>
+            <AccountEditPage />
+        </div>
     )
 }
