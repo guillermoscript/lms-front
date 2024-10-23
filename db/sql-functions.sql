9441544b0e85dbcbace51fcb49f0258fe8f9d5bb
guillermoscript <arepayquezo@gmail.com>
2024-10-23 13:39:38 -0400

    Add cron job to notify users for renewal

--- db/sql-functions.sql
+++ db/sql-functions.sql
@@ -500,3 +500,9 @@ $$;
 CREATE TRIGGER on_auth_user_created
 AFTER INSERT ON auth.users
 FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+
+SELECT cron.schedule(
+  'notify_users_for_renewal',
+  '0 0 * * *', -- Every day at midnight
+  'SELECT notify_users_for_renewal();'
+);
