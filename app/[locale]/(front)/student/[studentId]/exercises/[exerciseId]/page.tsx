117585cf7b1b8876e769930240a1a6aae43e458b
guillermoscript <arepayquezo@gmail.com>
2024-10-28 18:03:45 -0400

    Refactor ExerciseStudentPage component to fetch exercise data and student profile

---
+++ app/[locale]/(front)/student/[studentId]/exercises/[exerciseId]/page.tsx
@@ -0,0 +1,49 @@
+import { getI18n } from '@/app/locales/server'
+import { createClient } from '@/utils/supabase/server'
+import StudentPublicExercisePage from '@/components/front/StudentPublicExercisePage'
+
+export default async function ExerciseStudentPage({
+    params,
+}: {
+    params: { exerciseId: string; studentId: string }
+}) {
+    const supabase = createClient()
+
+    const exercise = await supabase
+        .from('exercises')
+        .select(
+            `*,
+            courses(title),
+            exercise_completions(*),
+            exercise_messages(id,message,role,created_at)`
+        )
+        .eq('id', params.exerciseId)
+        .eq('exercise_completions.user_id', params.studentId)
+        .eq('exercise_messages.user_id', params.studentId)
+        .order('created_at', {
+            referencedTable: 'exercise_messages',
+            ascending: true,
+        })
+        .single()
+
+
+    const profile = await supabase
+        .from('profiles')
+        .select('full_name, avatar_url')
+        .eq('id', params.studentId)
+        .single()
+
+    const t = await getI18n()
+
+    const isExerciseCompleted = exercise.data?.exercise_completions.length > 0
+
+    return (
+        <div className="md:container mx-auto space-y-4">
+            <StudentPublicExercisePage
+                exercise={exercise.data}
+                profile={profile.data}
+                isExerciseCompleted={isExerciseCompleted}
+            />
+        </div>
+    )
+}
