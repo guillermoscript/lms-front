92c7600a5d7d49def0b226597e27b0b740eb918e
guillermoscript <arepayquezo@gmail.com>
2024-11-01 18:01:45 -0400

    Refactor MySandpackEditor to conditionally render components based on completion status

--- components/dashboards/Common/Editor/MySandpackEditor.tsx
+++ components/dashboards/Common/Editor/MySandpackEditor.tsx
@@ -8,8 +8,6 @@ import {
 } from '@codesandbox/sandpack-react'
 import { useState } from 'react'
 
-import Confetti from '@/components/magicui/confetti'
-
 import SaveCode from '../../exercises/SaveCode'
 import CompletedMessage from './CompletedMessage'
 import MonacoEditor from './MonacoEditor'
@@ -33,27 +31,34 @@ export default function MySandpack({
 
     return (
         <>
-            {isCompleted && <Confetti />}
             <SaveCode isCompleted={isCompleted} exerciseId={exerciseId} />
-            <SandpackLayout>
-                <MonacoEditor
-                    autoHiddenFiles={autoHiddenFiles}
-                    readOnly={isCompleted}
-                    userCode={userCode}
-                />
-                <SandpackStack style={{ height: '100vh', margin: 0 }}>
-                    <SandpackPreview style={{ height: '70%' }} />
-                    {isCompleted ? (
-                        <CompletedMessage />
-                    ) : (
-                        <TestCompletionHandler
-                            exerciseId={exerciseId}
-                            code={code}
-                            setIsCompleted={setIsCompleted}
-                        />
-                    )}
-                </SandpackStack>
-            </SandpackLayout>
+            {!isCompleted && (
+                <SandpackLayout>
+                    <MonacoEditor
+                        readOnly={isCompleted}
+                        userCode={userCode}
+                    />
+                    <SandpackStack style={{ height: '100vh', margin: 0 }}>
+                        <>
+                            <SandpackPreview style={{ height: '70%' }} />
+                            <TestCompletionHandler
+                                exerciseId={exerciseId}
+                                code={code}
+                                setIsCompleted={setIsCompleted}
+                            />
+                        </>
+                    </SandpackStack>
+                </SandpackLayout>
+            )}
+            {isCompleted && (
+                <div className='flex flex-col gap-4 py-4'>
+                    <CompletedMessage />
+                    <MonacoEditor
+                        readOnly={isCompleted}
+                        userCode={userCode}
+                    />
+                </div>
+            )}
         </>
     )
 }
