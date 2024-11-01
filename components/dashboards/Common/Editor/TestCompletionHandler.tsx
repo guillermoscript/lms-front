e3df7a39e0ed4bac2b0dc38a1d4f5a9e1d2959f1
guillermoscript <arepayquezo@gmail.com>
2024-11-01 16:46:07 -0400

    Add TestCompletionHandler component for handling test results and submissions

---
+++ components/dashboards/Common/Editor/TestCompletionHandler.tsx
@@ -0,0 +1,61 @@
+import { SandpackTests, SandpackConsole } from '@codesandbox/sandpack-react'
+import axios from 'axios'
+
+interface TestCompletionHandlerProps {
+    exerciseId: number
+    code: string
+    setIsCompleted: (completed: boolean) => void
+}
+
+const allTestsPassed = (results: any) => {
+    for (const file in results) {
+        const fileResults = results[file]
+        for (const describeKey in fileResults.describes) {
+            const describe = fileResults.describes[describeKey]
+            for (const testKey in describe.tests) {
+                if (describe.tests[testKey].status !== 'pass') {
+                    return false
+                }
+            }
+        }
+        for (const testKey in fileResults.tests) {
+            if (fileResults.tests[testKey].status !== 'pass') {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+export default function TestCompletionHandler({
+    exerciseId,
+    code,
+    setIsCompleted,
+}: TestCompletionHandlerProps) {
+    return (
+        <>
+            <SandpackTests
+                style={{ height: '30%' }}
+                hideTestsAndSupressLogs
+                watchMode={false}
+                onComplete={async (results) => {
+                    if (allTestsPassed(results)) {
+                        try {
+                            const res = await axios.post('/api/exercises/code', {
+                                exerciseId,
+                                submissionCode: code,
+                            })
+                            console.log(res)
+                            setIsCompleted(true)
+                        } catch (error) {
+                            console.error('Error:', error)
+                        }
+                    } else {
+                        console.log('Some tests did not pass.')
+                    }
+                }}
+            />
+            <SandpackConsole />
+        </>
+    )
+}
