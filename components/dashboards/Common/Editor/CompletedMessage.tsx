7b44a0ab806f94f51cc7ecf2aa0af19b2fc80f34
guillermoscript <arepayquezo@gmail.com>
2024-11-01 18:01:59 -0400

    Refactor CompletedMessage component for improved layout and styling

--- components/dashboards/Common/Editor/CompletedMessage.tsx
+++ components/dashboards/Common/Editor/CompletedMessage.tsx
@@ -6,14 +6,14 @@ import { useScopedI18n } from '@/app/locales/client'
 export default function CompletedMessage() {
     const t = useScopedI18n('CompletedMessage')
     return (
-        <div className="flex items-center justify-center h-20">
+        <div className="flex flex-col gap-4 items-center justify-center">
             <div className="flex items-center space-x-2">
                 <h4 className="text-lg font-semibold text-green-500">
                     {t('title')}
                 </h4>
                 <Star className="w-6 h-6 text-yellow-500" />
             </div>
-            <p className="text-md">
+            <p className="text-md text-muted-foreground">
                 {t('description')}
             </p>
         </div>
