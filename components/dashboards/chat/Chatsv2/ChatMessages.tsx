2f1086274513aa1cc594d11bfacf27cbc11210d4
guillermoscript <arepayquezo@gmail.com>
2024-12-22 20:26:53 -0400

    messages from chat

---
+++ components/dashboards/chat/Chatsv2/ChatMessages.tsx
@@ -0,0 +1,40 @@
+import { Message } from 'ai'
+import { useEffect, useRef } from 'react'
+
+import ChatLoadingSkeleton from '@/components/dashboards/chat/ChatLoadingSkeleton'
+
+import { MessageItem } from './MessageItem'
+
+export default function ChatMessages({ messages, isLoading, reload, disabled }: {
+    messages: Message[]
+    isLoading: boolean
+    reload: (message: Message) => void
+    disabled: boolean
+}) {
+    const messagesEndRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+        }
+    }, [messages, isLoading])
+
+    const filteredMessages = messages.filter(message => message.role !== 'system')
+
+    return (
+        <div className="flex-1 overflow-y-auto w-full ">
+            {filteredMessages.map(message => (
+                <MessageItem
+                    reload={reload}
+                    isLoading={isLoading}
+                    key={message.id} message={message}
+                    disabled={disabled}
+                />
+            ))}
+            {isLoading && (
+                <ChatLoadingSkeleton />
+            )}
+            <div ref={messagesEndRef} />
+        </div>
+    )
+}
