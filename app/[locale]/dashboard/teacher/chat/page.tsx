0a53d89ca898b02153c005118e1652714dd09382
guillermoscript <arepayquezo@gmail.com>
2025-01-05 16:00:23 -0400

    refactor: update ChatPage component to use ChatContent and ExamLink for improved structure

--- app/[locale]/dashboard/teacher/chat/page.tsx
+++ app/[locale]/dashboard/teacher/chat/page.tsx
@@ -1,5 +1,6 @@
-import { FreeChatAI } from '@/actions/dashboard/AI/FreeChatPreparation'
-import FreeChat from '@/components/dashboards/chat/FreeChat'
+import { getScopedI18n } from '@/app/locales/server'
+import ChatContent from '@/components/dashboards/chat/Chatsv2/ChatContent'
+import ExamLink from '@/components/dashboards/chat/ExamLink'
 import FreeChatSetup from '@/components/dashboards/chat/tour/FreeChatSetup'
 import BreadcrumbComponent from '@/components/dashboards/student/course/BreadcrumbComponent'
 
@@ -7,6 +8,8 @@ export const dynamic = 'force-dynamic'
 export const maxDuration = 30
 
 export default async function ChatPage() {
+    const t = await getScopedI18n('BreadcrumbComponent')
+
     return (
         <>
             <BreadcrumbComponent
@@ -18,15 +21,15 @@ export default async function ChatPage() {
             />
             <div className="flex flex-col gap-4 w-full mb-8">
                 <div className="flex flex-wrap gap-4 w-full items-center">
-                    <h1 className="text-2xl font-semibold">
-                        Select a chat or start a new one
-                    </h1>
                     <FreeChatSetup />
                 </div>
+                <div className="flex flex-wrap gap-4 w-full">
+                    <ExamLink />
+                </div>
             </div>
-            <FreeChatAI>
-                <FreeChat />
-            </FreeChatAI>
+            <ChatContent
+                chatType="free_chat"
+            />
         </>
     )
 }
