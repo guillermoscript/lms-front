bccc33380de7bac4246b74778b1cabfcf7fc485e
guillermoscript <arepayquezo@gmail.com>
2025-02-22 20:35:12 -0400

    feat: enhance ShowCaseChat component with user assignment completion and improved input handling

--- components/dashboards/chat/ShowCaseChat.tsx
+++ components/dashboards/chat/ShowCaseChat.tsx
@@ -1,11 +1,15 @@
 'use client'
-import { useChat } from 'ai/react'
+import { useState } from 'react'
+import { useChat } from '@ai-sdk/react'
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 import MarkdownEditorTour from '../Common/tour/MarkdownEditorTour'
 import ChatLoadingSkeleton from './ChatLoadingSkeleton'
 import ChatMessages from './Chatsv2/ChatMessages'
+import MarkdowEditorInput from './Chatsv2/MarkdowEditorInput'
+import { ChatTextArea, SuccessMessage } from '../Common/chat/chat'
+import Confetti from '@/components/magicui/confetti'
 
 interface FreeChatProps {
     children?: React.ReactNode
@@ -13,7 +17,9 @@ interface FreeChatProps {
 }
 
 export default function ShowCaseChat({ systemPrompt }: FreeChatProps) {
-    const { messages, input, handleInputChange, handleSubmit, isLoading, reload, setInput, setMessages } = useChat({
+    const [completed, setCompleted] = useState(false)
+
+    const { messages, stop, append, isLoading, reload, setInput, setMessages } = useChat({
         initialMessages: [
             {
                 id: '1',
@@ -23,11 +29,19 @@ export default function ShowCaseChat({ systemPrompt }: FreeChatProps) {
             },
         ], // Set initial messages if any
         api: '/api/chat/home', // Update API endpoint as needed
+        maxSteps: 3, // Set max steps if needed
+        onToolCall({ toolCall }) {
+            // Handle tool call
+            console.log(toolCall)
+            if (toolCall.toolName === 'makeUserAssignmentCompleted') {
+                setCompleted(true)
+            }
+        }
     })
 
     return (
         <div className="h-full relative overflow-auto pt-4">
-            <div className="pb-[380px] lg:pb-[360px]">
+            <div className="py-4">
                 {messages.length > 1 ? (
                     <ChatMessages messages={messages}
                         isLoading={isLoading}
@@ -40,41 +54,64 @@ export default function ShowCaseChat({ systemPrompt }: FreeChatProps) {
                 {isLoading && <ChatLoadingSkeleton />}
                 <div className="w-full h-px" />
             </div>
-            <div className='w-full absolute bottom-0'>
-                <Tabs defaultValue="simple" className="w-full py-4">
-                    <div className="flex gap-4">
-                        <TabsList
-                            id='tabs-list'
-                            className='gap-4'
-                        >
-                            <TabsTrigger
-                                id='simple-tab'
-                                value="simple"
+            <div className='w-full '>
+                {completed ? (
+                    <>
+                        <SuccessMessage />
+                    </>
+                ) : (
+                    <Tabs defaultValue="simple" className="w-full py-4">
+                        <div className="flex gap-4">
+                            <TabsList
+                                id='tabs-list'
+                                className='gap-4'
                             >
+                                <TabsTrigger
+                                    id='simple-tab'
+                                    value="simple"
+                                >
                                     Simple
-                            </TabsTrigger>
-                            <TabsTrigger
-                                id='markdown-tab'
-                                value="markdown"
-                            >
+                                </TabsTrigger>
+                                <TabsTrigger
+                                    id='markdown-tab'
+                                    value="markdown"
+                                >
                                     Markdown
-                            </TabsTrigger>
-                        </TabsList>
-                        <MarkdownEditorTour />
-                    </div>
-                    <TabsContent
-                        id='markdown-content'
-                        value="markdown"
-                    >
-
-                    </TabsContent>
-                    <TabsContent
-                        id='simple-content'
-                        value="simple"
-                    >
-
-                    </TabsContent>
-                </Tabs>
+                                </TabsTrigger>
+                            </TabsList>
+                            <MarkdownEditorTour />
+                        </div>
+                        <TabsContent
+                            id='markdown-content'
+                            value="markdown"
+                        >
+                            <MarkdowEditorInput
+                                isLoading={isLoading}
+                                stop={() => stop()}
+                                callbackFunction={(message) => {
+                                    // handleSendMessage(message.content)
+                                    console.log(message)
+                                    append(message)
+                                }}
+                                isTemplatePresent={true}
+                            />
+                        </TabsContent>
+                        <TabsContent
+                            id='simple-content'
+                            value="simple"
+                        >
+                            <ChatTextArea
+                                isLoading={isLoading}
+                                stop={() => stop()}
+                                callbackFunction={async (message) => {
+                                    // handleSendMessage(message.content)
+                                    console.log(message)
+                                    append(message)
+                                }}
+                            />
+                        </TabsContent>
+                    </Tabs>
+                )}
             </div>
         </div>
     )
