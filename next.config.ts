94d0df36adac338e60cbab9e3a28f05f70dfb183
Guillermo Marin <52298929+guillermoscript@users.noreply.github.com>
2026-02-20 00:09:34 +0100

    feat: add Dockerfile and .dockerignore for containerization setup

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
